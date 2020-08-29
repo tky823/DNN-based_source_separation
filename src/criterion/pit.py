919f881e7f56850e2bdd09ae099099f5b2416521
tky1117 <delta9guitar97@gmail.com>
2020-08-30 00:44:51 +0900

    Added SI-SDR & PIT

---
+++ src/criterion/pit.py
@@ -0,0 +1,45 @@
+import itertools
+import torch
+
+"""
+    Permutation invariant training
+"""
+
+class PIT1d:
+    def __init__(self, criterion, n_sources):
+        """
+        Args:
+            criterion <callable>: criterion is expected acceptable (input, target, batch_mean) when called.
+        """
+        self.criterion = criterion
+        patterns = list(itertools.permutations(range(n_sources)))
+        self.patterns = torch.Tensor(patterns).long()
+        
+    def __call__(self, input, target, batch_mean=True):
+        """
+        Args:
+            input (batch_size, n_sources, *)
+            target (batch_size, n_sources, *)
+        Returns:
+            loss (batch_size,): minimum loss for each data
+            patterns (batch_size,): permutation indices
+        """
+        criterion = self.criterion
+        patterns = self.patterns
+        P = len(patterns)
+        possible_loss = None
+        
+        for idx in range(P):
+            pattern = patterns[idx]
+            loss = self.criterion(input, target[:,pattern], batch_mean=False)
+            if possible_loss is None:
+                possible_loss = loss.unsqueeze(dim=1)
+            else:
+                possible_loss = torch.cat([possible_loss, loss.unsqueeze(dim=1)], dim=1)
+                
+        # possible_loss (batch_size, P)
+        loss, idx_min = torch.min(possible_loss, dim=1) # loss (batch_size, ), idx_min (batch_size, )
+        if batch_mean:
+            loss = loss.mean(dim=0)
+             
+        return loss, patterns[idx_min]
---
+++ src/criterion/sdr.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+
+EPS=1e-12
+
+"""
+    Scale-invariant-SDR (source-to-distortion ratio)
+    See "SDR - half-baked or well done?"
+    https://arxiv.org/abs/1811.02508
+"""
+
+def si_sdr(input, target, eps=EPS):
+    """
+    Scale-invariant-SDR (source-to-distortion ratio)
+    Args:
+        input (batch_size, T) or (batch_size, C, T)
+        target (batch_size, T) or (batch_size, C, T)
+    Returns:
+        loss (batch_size,) or (batch_size, C)
+    """
+    n_dim = input.dim()
+    
+    assert n_dim in [2, 3], "Only 2D or 3D tensor is acceptable, but given {}D tensor.".format(n_dim)
+
+    alpha = torch.sum(input * target, dim=n_dim-1, keepdim=True) / (torch.sum(target**2, dim=n_dim-1, keepdim=True) + eps)
+    loss = (torch.sum((alpha * target)**2, dim=n_dim-1) + eps) / (torch.sum((alpha * target - input)**2, dim=n_dim-1) + eps)
+    loss = 10 * torch.log10(loss)
+
+    return loss
+
+class SISDR(nn.Module):
+    def __init__(self, eps=EPS):
+        super().__init__()
+        
+        self.eps = eps
+        
+    def forward(self, input, target, batch_mean=True):
+        """
+        Args:
+            input (batch_size, T) or (batch_size, C, T)
+            target (batch_size, T) or (batch_size, C, T)
+        Returns:
+            loss (batch_size,) or (batch_size, C)
+        """
+        n_dim = input.dim()
+        
+        assert n_dim in [2, 3], "Only 2D or 3D tensor is acceptable, but given {}D tensor.".format(n_dim)
+        
+        loss = si_sdr(input, target, eps=self.eps)
+        
+        if n_dim == 3:
+            loss = loss.mean(dim=1)
+        
+        if batch_mean:
+            loss = loss.mean(dim=0)
+        
+        return loss
+        
+class NegSISDR(nn.Module):
+    def __init__(self, eps=EPS):
+        super().__init__()
+        
+        self.eps = eps
+        
+    def forward(self, input, target, batch_mean=True):
+        """
+        Args:
+            input (batch_size, T) or (batch_size, C, T)
+            target (batch_size, T) or (batch_size, C, T)
+        Returns:
+            loss (batch_size,)
+        """
+        n_dim = input.dim()
+        
+        assert n_dim in [2, 3], "Only 2D or 3D tensor is acceptable, but given {}D tensor.".format(n_dim)
+        
+        loss = - si_sdr(input, target, eps=self.eps)
+        
+        if n_dim == 3:
+            loss = loss.mean(dim=1)
+        
+        if batch_mean:
+            loss = loss.mean(dim=0)
+        
+        return loss
