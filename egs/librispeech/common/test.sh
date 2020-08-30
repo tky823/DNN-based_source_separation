f87ccdf128089e5a68a08947cb8ac7c04b5c1e81
tky1117 <delta9guitar97@gmail.com>
2020-08-30 15:52:52 +0900

    Added PESQ

--- egs/librispeech/common/test.sh
+++ egs/librispeech/common/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+out_dir="$1"
+
 echo "download from https://www.itu.int/rec/T-REC-P.862-200102-I/en"
 
 gcc -c dsp.c
@@ -8,4 +10,8 @@ gcc -c pesqmod.c
 gcc -c pesqio.c
 gcc -c pesqmain.c
 
-gcc dsp.o pesqdsp.o pesqmain.o pesqmod.o pesqio.o -o PESQ -lm
+if [ -z ${out_dir} ]; then
+    out_dir="."
+fi
+
+gcc dsp.o pesqdsp.o pesqmain.o pesqmod.o pesqio.o -o ${out_dir}/PESQ -lm
