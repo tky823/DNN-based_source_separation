e534d4a0abf444d34ff0c64966d05a1fcc799517
tky1117 <delta9guitar97@gmail.com>
2020-08-31 01:39:55 +0900

    Added DANet

---
+++ egs/librispeech/danet/local/train.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import torch
+import torch.nn as nn
+
+from utils.utils import set_seed
+from dataset import SpectrogramTrainDataset, TrainDataLoader
+from driver import Trainer
+from models.danet import DANet
+from criterion.distance import L2Loss
+from criterion.pit import PIT2d
+
+parser = argparse.ArgumentParser(description="Training of ADANet")
+
+parser.add_argument('--wav_root', type=str, default=None, help='Path for dataset ROOT directory')
+parser.add_argument('--train_json_path', type=str, default=None, help='Path for train.json')
+parser.add_argument('--valid_json_path', type=str, default=None, help='Path for valid.json')
+parser.add_argument('--sr', type=int, default=10, help='Sampling rate')
+parser.add_argument('--window_fn', type=str, default='hamming', help='Window function')
+# Model configuration
+parser.add_argument('--fft_size', type=int, default=256, help='Window length')
+parser.add_argument('--embed_dim', '-K', type=int, default=20, help='Embedding dimension')
+parser.add_argument('--hidden_channels', '-H', type=int, default=600, help='hidden_channels')
+parser.add_argument('--num_blocks', '-B', type=int, default=4, help='# LSTM layers')
+parser.add_argument('--causal', type=int, default=0, help='Causality')
+parser.add_argument('--mask_nonlinear', type=str, default='sigmoid', help='Non-linear function of mask estiamtion')
+parser.add_argument('--n_sources', type=int, default=None, help='# speakers')
+parser.add_argument('--criterion', type=str, default='sisdr', choices=['sisdr'], help='Criterion')
+parser.add_argument('--optimizer', type=str, default='adam', choices=['sgd', 'adam'], help='Optimizer, [sgd, adam]')
+parser.add_argument('--lr', type=float, default=0.001, help='Learning rate. Default: 0.001')
+parser.add_argument('--weight_decay', type=float, default=0, help='Weight decay (L2 penalty). Default: 0')
+parser.add_argument('--max_norm', type=float, default=None, help='Gradient clipping')
+
+parser.add_argument('--batch_size', type=int, default=4, help='Batch size. Default: 128')
+parser.add_argument('--epochs', type=int, default=5, help='Number of epochs')
+parser.add_argument('--model_dir', type=str, default='./tmp/model', help='Model directory')
+parser.add_argument('--loss_dir', type=str, default='./tmp/loss', help='Loss directory')
+parser.add_argument('--sample_dir', type=str, default='./tmp/sample', help='Sample directory')
+parser.add_argument('--continue_from', type=str, default=None, help='Resume training')
+parser.add_argument('--use_cuda', type=int, default=1, help='0: Not use cuda, 1: Use cuda')
+parser.add_argument('--overwrite', type=int, default=0, help='0: NOT overwrite, 1: FORCE overwrite')
+parser.add_argument('--seed', type=int, default=42, help='Random seed')
+
+def main(args):
+    set_seed(args.seed)
+    
+    loader = {}
+    
+    train_dataset = SpectrogramTrainDataset(args.wav_root, args.train_json_path)
+    valid_dataset = SpectrogramTrainDataset(args.wav_root, args.valid_json_path)
+    print("Training dataset includes {} samples.".format(len(train_dataset)))
+    print("Valid dataset includes {} samples.".format(len(valid_dataset)))
+    
+    loader['train'] = TrainDataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    loader['valid'] = TrainDataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False)
+    
+    F_bin = fft_size//2 + 1
+    model = DANet(F_bin, embed_dim=args.embed_dim, hidden_channels=args.hidden_channels, num_blocks=args.num_blocks, causal=args.causal, mask_nonlinear=args.mask_nonlinear, n_sources=args.n_sources)
+    
+    for mixture, sources in loader['train']:
+        print(mixture.size(), sources.size())
+        raise ValueError("Stop")
+    
+    if args.use_cuda:
+        if torch.cuda.is_available():
+            model.cuda()
+            model = nn.DataParallel(model)
+            print("Use CUDA")
+        else:
+            raise ValueError("Cannot use CUDA.")
+    else:
+        print("Does NOT use CUDA")
+    
+    # Optimizer
+    if args.optimizer == 'sgd':
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+    elif args.optimizer == 'adam':
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    else:
+        raise ValueError("Not support optimizer {}".format(args.optimizer))
+        
+    # Criterion
+    if args.criterion == 'l2loss':
+        criterion = L2Loss(reduction=)
+    else:
+        raise ValueError("Not support criterion {}".format(args.criterion))
+    
+    pit_criterion = PIT2d(criterion, n_sources=args.n_sources)
+    
+    trainer = Trainer(model, loader, pit_criterion, optimizer, args)
+    trainer.run()
+    
+    
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)
+    main(args)
---
+++ src/models/danet.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+
+EPS=1e-12
+
+class DANet(nn.Module):
+    def __init__(self, F_bin, embed_dim=20, hidden_channels=600, num_blocks=4, causal=False, mask_nonlinear='sigmoid', n_sources=2, eps=EPS, **kwargs):
+        super().__init__()
+        
+        self.embed_dim = embed_dim
+        self.num_blocks = num_blocks
+        
+        self.lstm = StackedLSTM(F_bin, hidden_channels=hidden_channels, num_blocks=num_blocks, causal=causal)
+        self.fc = nn.Linear(hidden_channels, F_bin*embed_dim)
+        
+        if mask_nonlinear == 'sigmoid':
+            self.mask_nonlinear = nn.Sigmoid()
+        elif mask_nonlinear == 'softmax':
+            self.mask_nonlinear = nn.Softmax(dim=1)
+        else:
+            raise NotImplementedError("")
+    
+        self.n_sources = n_sources
+        
+        self.num_parameters = self._get_num_parameters()
+    
+    def forward(self, input, assignment, threshold=None):
+        """
+        Args:
+            input (batch_size, 1, F_bin, T_bin)
+            assignment (batch_size, n_sources, F_bin, T_bin): Speaker assignment when training
+        Returns:
+            output (batch_size, n_sources, F_bin, T_bin)
+        """
+        embed_dim = self.embed_dim
+        n_sources = self.n_sources
+        
+        batch_size, _, F_bin, T_bin = input.size()
+        
+        x = self.lstm(input) # -> (batch_size, T_bin, F_bin)
+        x = self.fc(x) # -> (batch_size, T_bin, embed_dim*F_bin)
+        x = x.view(batch_size, T_bin, embed_dim, F_bin)
+        x = x.permute(0,2,3,1).contiguous()  # -> (batch_size, embed_dim, F_bin, T_bin)
+        x = x.view(batch_size, embed_dim, F_bin*T_bin)
+        
+        input = input.view(batch_size, 1, F_bin*T_bin)
+        assignment = assignment.view(batch_size, n_sources, F_bin*T_bin) # -> (batch_size, n_sources, F_bin*T_bin)
+        
+        if not self.training:
+            if threshold is None:
+                raise ValueError("Specify threshold!")
+            
+            w = torch.where(input>threshold, torch.ones_like(input), torch.zeros_like(input))  # -> (batch_size, 1, F_bin*T_bin)
+            assignment = w * assignment
+        
+        attractor = torch.bmm(assignment, x.permute(0,2,1)) / assignment.sum(dim=2, keepdim=True) # -> (batch_size, n_sources, K)
+        
+        similarity = torch.bmm(attractor, x) # -> (batch_size, n_sources, F_bin*T_bin)
+        similarity = similarity.view(batch_size, n_sources, F_bin, T_bin)
+        mask = self.mask_nonlinear(similarity) # -> (batch_size, n_sources, F_bin, T_bin)
+        
+        return mask
+    
+    def _get_num_parameters(self):
+        num_parameters = 0
+        
+        for p in self.parameters():
+            if p.requires_grad:
+                num_parameters += p.numel()
+                
+        return num_parameters
+
+class StackedLSTM(nn.Module):
+    def __init__(self, F_bin, hidden_channels=600, num_blocks=4, causal=False):
+        super().__init__()
+        
+        net = []
+        
+        self.num_blocks = num_blocks
+        
+        if causal:
+            bidirectional = False
+            num_directions = 1
+        else:
+            bidirectional = True
+            num_directions = 2
+        
+        for idx in range(num_blocks):
+            if idx == 0:
+                in_channels = F_bin
+                out_channels = hidden_channels//num_directions
+            else:
+                in_channels = hidden_channels
+                out_channels = hidden_channels//num_directions
+            
+            net.append(nn.LSTM(in_channels, out_channels, batch_first=True, bidirectional=bidirectional))
+        
+        self.net = nn.Sequential(*net)
+        
+    def forward(self, input):
+        """
+        Args:
+            input (batch_size, 1, F_bin, T_bin)
+        Returns:
+            output (batch_size, T_bin, F_bin)
+        """
+        x = input.squeeze(dim=1).permute(0,2,1) # -> (batch_size, T_bin, F_bin)
+        
+        for idx in range(self.num_blocks):
+            # To avoid error
+            x, (_, _) = self.net[idx](x)
+        
+        output = x
+
+        return output
+        
+if __name__ == '__main__':
+    from algorithm.ideal_mask import ideal_binary_mask
+    
+    torch.manual_seed(111)
+    
+    batch_size = 2
+    K = 10
+    
+    H = 32
+    B = 4
+    
+    F_bin, T_bin = 4, 128
+    C = 2
+    causal = False
+    mask_nonlinear = 'sigmoid'
+    
+    sources = torch.randint(0, 10, (batch_size, C, F_bin, T_bin), dtype=torch.float)
+    input = sources.sum(dim=1, keepdim=True)
+    assignment = ideal_binary_mask(sources)
+    
+    model = DANet(F_bin, embed_dim=K, hidden_channels=H, num_blocks=B, causal=causal, mask_nonlinear=mask_nonlinear, n_sources=C)
+    print(model)
+    output = model(input, assignment)
+    
+    print(input.size(), output.size())
