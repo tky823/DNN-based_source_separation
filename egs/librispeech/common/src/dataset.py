cd862c0b9f37f6a5ba5cbb60e79a374d7d156a56
tky1117 <delta9guitar97@gmail.com>
2020-08-30 15:41:05 +0900

    Added assertion

--- egs/librispeech/common/src/dataset.py
+++ egs/librispeech/common/src/dataset.py
@@ -108,6 +108,22 @@ class TestDataLoader(torch.utils.data.DataLoader):
 
 def test_collate_fn(batch):
     print(batch)
+    batched_mixture, batched_sources = None, None
+    batched_segment_ID = []
+    
+    for mixture, sources, segmend_ID in batch:
+        mixture = mixture.unsqueeze(dim=0)
+        sources = sources.unsqueeze(dim=0)
+        
+        if batched_mixture is None:
+            batched_mixture = mixture
+            batched_sources = sources
+        else:
+            batched_mixture = torch.cat([batched_mixture, mixture], dim=0)
+            batched_sources = torch.cat([batched_sources, sources], dim=0)
+        batched_segment_ID.append(segmend_ID)
+    
+    print(batched_segment_ID)
     
     raise ValueError("Stop")
 
