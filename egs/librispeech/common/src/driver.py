b699e8292155516be8afa0074cd0e15b7a262f70
tky1117 <delta9guitar97@gmail.com>
2020-08-31 15:24:30 +0900

    Added assertion

--- egs/librispeech/common/src/driver.py
+++ egs/librispeech/common/src/driver.py
@@ -407,6 +407,7 @@ class AttractorTrainer(Trainer):
                     real, imag = mixture[:,:F_bin], mixture[:,F_bin:]
                     real, imag = ratio * real, ratio * imag
                     estimated_sources = torch.cat([real, imag], dim=0) # -> (n_sources, 2*F_bin, T_bin)
+                    print(estimated_sources.size())
                     estimated_sources = self.istft(estimated_sources) # -> (n_sources, T)
                     estimated_sources = estimated_sources.detach().cpu().numpy()
                     
