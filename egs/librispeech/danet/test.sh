25165ab4e2bdfc7cd0deccf170fac045ca49056f
tky1117 <delta9guitar97@gmail.com>
2020-09-02 08:33:47 +0900

    Added test script

---
+++ egs/librispeech/danet/test.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+. ./path.sh
+
+exp_dir="$1"
+
+n_sources=2
+
+wav_root="../../../dataset/LibriSpeech"
+test_json_path="../../../dataset/LibriSpeech/test-clean/test-${n_sources}mix.json"
+
+sr=16000
+
+window_fn='hamming'
+fft_size=256
+hop_size=64
+ideal_mask='ibm'
+threshold=40
+
+# Model configuration
+K=20
+H=256
+B=4
+causal=0
+mask_nonlinear='sigmoid'
+
+# Criterion
+criterion='l2loss'
+
+# Optimizer
+optimizer='rmsprop'
+lr=1e-4
+weight_decay=0
+
+batch_size=4
+epochs=100
+
+use_cuda=0
+overwrite=0
+seed=111
+
+prefix=""
+
+save_dir="${exp_dir}/${n_sources}mix/${criterion}/${window_fn}-window_${ideal_mask}_threshold${threshold}/K${K}_H${H}_B${B}_H${H}_causal${causal}_mask-${mask_nonlinear}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-decay${weight_decay}/seed${seed}"
+
+model_choice="best"
+
+model_dir="${save_dir}/model"
+model_path="${model_dir}/${model_choice}.pth"
+log_dir="${save_dir}/log"
+out_dir="${save_dir}/test"
+
+if [ ! -e "${log_dir}" ]; then
+    mkdir -p "${log_dir}"
+fi
+
+time_stamp=`TZ=UTC-9 date "+%Y%m%d-%H%M%S"`
+
+test.py \
+--wav_root ${wav_root} \
+--test_json_path ${test_json_path} \
+--sr ${sr} \
+--window_fn ${window_fn} \
+--ideal_mask ${ideal_mask} \
+--threshold ${threshold} \
+--fft_size ${fft_size} \
+--hop_size ${hop_size} \
+--n_sources ${n_sources} \
+--criterion ${criterion} \
+--out_dir "${out_dir}" \
+--model_path "${model_path}" \
+--use_cuda ${use_cuda} \
+--overwrite ${overwrite} \
+--seed ${seed} | tee "${log_dir}/test_${time_stamp}.log"
