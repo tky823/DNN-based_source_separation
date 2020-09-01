876792016e1ada4bfa3f8cc712c259887baa9829
tky1117 <delta9guitar97@gmail.com>
2020-09-01 19:07:12 +0900

    Added PIT method

--- src/criterion/pit.py
+++ src/criterion/pit.py
@@ -4,6 +4,44 @@ import torch
 """
     Permutation invariant training
 """
+def pit(criterion, input, target, n_sources=None, patterns=None):
+    """
+    Args:
+        criterion <callable>
+        input (batch_size, n_sources, *)
+        output (batch_size, n_sources, *)
+    Returns:
+        loss (batch_size,): minimum loss for each data
+        pattern (batch_size,): permutation indices
+    """
+    if patterns is None:
+        if n_sources is None:
+            n_sources = input.size(1)
+        patterns = list(itertools.permutations(range(n_sources)))
+        patterns = torch.Tensor(patterns).long()
+    
+    P = len(patterns)
+    possible_loss = None
+    
+    for idx in range(P):
+        pattern = patterns[idx]
+        loss = criterion(input, target[:,pattern], batch_mean=False)
+        if possible_loss is None:
+            possible_loss = loss.unsqueeze(dim=1)
+        else:
+            possible_loss = torch.cat([possible_loss, loss.unsqueeze(dim=1)], dim=1)
+            
+    # possible_loss (batch_size, P)
+    if criterion.maximize:
+        loss, indices = torch.max(possible_loss, dim=1) # loss (batch_size,), indices (batch_size,)
+    else:
+        loss, indices = torch.min(possible_loss, dim=1) # loss (batch_size,), indices (batch_size,)
+    
+    if batch_mean:
+        loss = loss.mean(dim=0)
+         
+    return loss, patterns[indices]
+        
 
 class PIT:
     def __init__(self, criterion, n_sources):
@@ -22,30 +60,11 @@ class PIT:
             target (batch_size, n_sources, *)
         Returns:
             loss (batch_size,): minimum loss for each data
-            patterns (batch_size,): permutation indices
+            pattern (batch_size,): permutation indices
         """
-        criterion = self.criterion
-        patterns = self.patterns
-        P = len(patterns)
-        possible_loss = None
-        
-        for idx in range(P):
-            pattern = patterns[idx]
-            loss = self.criterion(input, target[:,pattern], batch_mean=False)
-            if possible_loss is None:
-                possible_loss = loss.unsqueeze(dim=1)
-            else:
-                possible_loss = torch.cat([possible_loss, loss.unsqueeze(dim=1)], dim=1)
-                
-        # possible_loss (batch_size, P)
-        if self.criterion.maximize:
-            loss, idx_max = torch.max(possible_loss, dim=1) # loss (batch_size, ), idx_min (batch_size, )s
-        else:
-            loss, idx_min = torch.min(possible_loss, dim=1) # loss (batch_size, ), idx_min (batch_size, )
-        if batch_mean:
-            loss = loss.mean(dim=0)
+        loss, pattern = pit(self.criterion, input, target, patterns=self.patterns)
              
-        return loss, patterns[idx_min]
+        return loss, pattern
 
 class PIT1d(PIT):
     def __init__(self, criterion, n_sources):
