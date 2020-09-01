4e6f9c0dd49c4e5972c66461b6a282718af17b19
tky1117 <delta9guitar97@gmail.com>
2020-09-01 20:22:15 +0900

    Added evaluation dataset & loader

--- egs/librispeech/danet/local/train.py
+++ egs/librispeech/danet/local/train.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 
 from utils.utils import set_seed
-from dataset import IdealMaskSpectrogramDataset, TrainDataLoader
+from dataset import IdealMaskSpectrogramDataset, EvalIdealMaskSpectrogramDataset, TrainDataLoader, EvalDataLoader
 from driver import AttractorTrainer
 from models.danet import DANet
 from criterion.distance import L2Loss
@@ -51,11 +51,13 @@ def main(args):
     
     train_dataset = IdealMaskSpectrogramDataset(args.wav_root, args.train_json_path, fft_size=args.fft_size, hop_size=args.hop_size, window_fn=args.window_fn, mask_type=args.ideal_mask, threshold=args.threshold)
     valid_dataset = IdealMaskSpectrogramDataset(args.wav_root, args.valid_json_path, fft_size=args.fft_size, hop_size=args.hop_size, window_fn=args.window_fn, mask_type=args.ideal_mask, threshold=args.threshold)
+    # valid_dataset = EvalIdealMaskSpectrogramDataset(args.wav_root, args.valid_json_path, fft_size=args.fft_size, hop_size=args.hop_size, window_fn=args.window_fn, mask_type=args.ideal_mask, threshold=args.threshold)
     print("Training dataset includes {} samples.".format(len(train_dataset)))
     print("Valid dataset includes {} samples.".format(len(valid_dataset)))
     
     loader['train'] = TrainDataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
     loader['valid'] = TrainDataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False)
+    # loader['valid'] = EvalDataLoader(valid_dataset, batch_size=1, shuffle=False)
     
     args.F_bin = args.fft_size//2 + 1
     model = DANet(args.F_bin, embed_dim=args.embed_dim, hidden_channels=args.hidden_channels, num_blocks=args.num_blocks, causal=args.causal, mask_nonlinear=args.mask_nonlinear, n_sources=args.n_sources)
