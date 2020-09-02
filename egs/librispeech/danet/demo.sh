ec01798f0145bd5e2f8ad1c6ae4bc7a3f8a5b1b8
tky1117 <delta9guitar97@gmail.com>
2020-09-02 20:53:50 +0900

    Changed output directory

--- egs/librispeech/danet/demo.sh
+++ egs/librispeech/danet/demo.sh
@@ -6,9 +6,6 @@ exp_dir="$1"
 
 n_sources=2
 
-wav_root="../../../dataset/LibriSpeech"
-test_json_path="../../../dataset/LibriSpeech/test-clean/test-${n_sources}mix.json"
-
 sr=16000
 
 window_fn='hamming'
@@ -35,40 +32,28 @@ weight_decay=0
 batch_size=4
 epochs=100
 
-use_cuda=0
+use_cuda=1
 overwrite=0
 seed=111
 
-prefix=""
+if [ -z "${exp_dir}" ]; then
+    exp_dir="./exp"
+fi
 
 save_dir="${exp_dir}/${n_sources}mix/${criterion}/stft${fft_size}-${hop_size}_${window_fn}-window_${ideal_mask}_threshold${threshold}/K${K}_H${H}_B${B}_H${H}_causal${causal}_mask-${mask_nonlinear}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-decay${weight_decay}/seed${seed}"
 
 model_choice="best"
+model_path="${save_dir}/model/${model_choice}.pth"
 
-model_dir="${save_dir}/model"
-model_path="${model_dir}/${model_choice}.pth"
-log_dir="${save_dir}/log"
-out_dir="${save_dir}/test"
+num_chunk=256
+duration=5
 
-if [ ! -e "${log_dir}" ]; then
-    mkdir -p "${log_dir}"
-fi
-
-time_stamp=`TZ=UTC-9 date "+%Y%m%d-%H%M%S"`
-
-test.py \
---wav_root ${wav_root} \
---test_json_path ${test_json_path} \
+demo.py \
 --sr ${sr} \
 --window_fn ${window_fn} \
---ideal_mask ${ideal_mask} \
---threshold ${threshold} \
 --fft_size ${fft_size} \
 --hop_size ${hop_size} \
---n_sources ${n_sources} \
---criterion ${criterion} \
---out_dir "${out_dir}" \
---model_path "${model_path}" \
---use_cuda ${use_cuda} \
---overwrite ${overwrite} \
---seed ${seed} | tee "${log_dir}/test_${time_stamp}.log"
+--num_chunk ${num_chunk} \
+--duration ${duration} \
+--model_path ${model_path} \
+--save_dir "./results"
