b409eb771d89bd1ba26a697de9c632955d852737
tky1117 <delta9guitar97@gmail.com>
2020-09-12 20:14:06 +0900

    Add optimizer option

--- egs/librispeech/adanet/local/train.py
+++ egs/librispeech/adanet/local/train.py
@@ -6,24 +6,35 @@ import torch
 import torch.nn as nn
 
 from utils.utils import set_seed
-from dataset import SpectrogramTrainDataset, TrainDataLoader
-from driver import Trainer
+from dataset import IdealMaskSpectrogramTrainDataset, IdealMaskSpectrogramEvalDataset, TrainDataLoader, EvalDataLoader
+from driver import AttractorTrainer
+from models.adanet import ADANet
+from criterion.distance import L2Loss
 
-parser = argparse.ArgumentParser(description="Training of ADANet")
+parser = argparse.ArgumentParser(description="Training of ADANet (Anchored Deep Attractor Network)")
 
 parser.add_argument('--wav_root', type=str, default=None, help='Path for dataset ROOT directory')
 parser.add_argument('--train_json_path', type=str, default=None, help='Path for train.json')
 parser.add_argument('--valid_json_path', type=str, default=None, help='Path for valid.json')
 parser.add_argument('--sr', type=int, default=10, help='Sampling rate')
 parser.add_argument('--window_fn', type=str, default='hamming', help='Window function')
+parser.add_argument('--ideal_mask', type=str, default='ibm', choices=['ibm', 'irm', 'wfm'], help='Ideal mask for assignment')
+parser.add_argument('--threshold', type=float, default=40, help='Wight threshold. Default: 40 ')
+
 # Model configuration
+parser.add_argument('--fft_size', type=int, default=256, help='Window length')
+parser.add_argument('--hop_size', type=int, default=None, help='Hop size')
+parser.add_argument('--embed_dim', '-K', type=int, default=20, help='Embedding dimension')
+parser.add_argument('--hidden_channels', '-H', type=int, default=600, help='hidden_channels')
+parser.add_argument('--num_blocks', '-B', type=int, default=4, help='# LSTM layers')
+parser.add_argument('--causal', type=int, default=0, help='Causality')
+parser.add_argument('--mask_nonlinear', type=str, default='sigmoid', help='Non-linear function of mask estiamtion')
 parser.add_argument('--n_sources', type=int, default=None, help='# speakers')
 parser.add_argument('--criterion', type=str, default='sisdr', choices=['sisdr'], help='Criterion')
-parser.add_argument('--optimizer', type=str, default='adam', choices=['sgd', 'adam'], help='Optimizer, [sgd, adam]')
+parser.add_argument('--optimizer', type=str, default='adam', choices=['sgd', 'adam', 'rmsprop'], help='Optimizer, [sgd, adam, rmsprop]')
 parser.add_argument('--lr', type=float, default=0.001, help='Learning rate. Default: 0.001')
 parser.add_argument('--weight_decay', type=float, default=0, help='Weight decay (L2 penalty). Default: 0')
 parser.add_argument('--max_norm', type=float, default=None, help='Gradient clipping')
-
 parser.add_argument('--batch_size', type=int, default=4, help='Batch size. Default: 128')
 parser.add_argument('--epochs', type=int, default=5, help='Number of epochs')
 parser.add_argument('--model_dir', type=str, default='./tmp/model', help='Model directory')
