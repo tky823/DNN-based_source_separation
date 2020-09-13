511baebfbb0089593a490a5f59524c3a6f5fc2f3
tky1117 <delta9guitar97@gmail.com>
2020-09-13 11:50:08 +0900

    Add driver for ADANet

--- egs/librispeech/common/src/driver.py
+++ egs/librispeech/common/src/driver.py
@@ -586,3 +586,122 @@ class AttractorTester(Tester):
                 
         print("Loss: {:.3f}, PESQ: {:.3f}".format(test_loss, test_pesq))
         
+class AnchoredAttractorTrainer(AttractorTrainer):
+    def __init__(self, model, loader, criterion, optimizer, args):
+        super().__init__(model, loader, criterion, optimizer, args)
+    
+    def run_one_epoch_train(self, epoch):
+        # Override
+        """
+        Training
+        """
+        F_bin = self.F_bin
+        
+        self.model.train()
+        
+        train_loss = 0
+        n_train_batch = len(self.train_loader)
+        
+        for idx, (mixture, sources, threshold_weight) in enumerate(self.train_loader):
+            if self.use_cuda:
+                mixture = mixture.cuda()
+                sources = sources.cuda()
+                assignment = assignment.cuda()
+                threshold_weight = threshold_weight.cuda()
+                
+            real, imag = mixture[:,:,:F_bin], mixture[:,:,F_bin:]
+            mixture_amplitude = torch.sqrt(real**2+imag**2)
+            real, imag = sources[:,:,:F_bin], sources[:,:,F_bin:]
+            sources_amplitude = torch.sqrt(real**2+imag**2)
+            
+            estimated_sources_amplitude = self.model(mixture_amplitude, assignment=assignment, threshold_weight=threshold_weight, n_sources=assignment.size(1))
+            loss = self.criterion(estimated_sources_amplitude, sources_amplitude)
+            
+            self.optimizer.zero_grad()
+            loss.backward()
+            
+            if self.max_norm:
+                nn.utils.clip_grad_norm_(self.model.parameters(), self.max_norm)
+            
+            self.optimizer.step()
+        
+            train_loss += loss.item()
+            
+            if (idx + 1)%100 == 0:
+                print("[Epoch {}/{}] iter {}/{} loss: {:.5f}".format(epoch+1, self.epochs, idx+1, n_train_batch, loss.item()), flush=True)
+        
+        train_loss /= n_train_batch
+        
+        return train_loss
+    
+    def run_one_epoch_eval(self, epoch):
+        # Override
+        """
+        Validation
+        """
+        n_sources = self.n_sources
+        F_bin = self.F_bin
+        
+        self.model.eval()
+        
+        valid_loss = 0
+        n_valid = len(self.valid_loader.dataset)
+        
+        with torch.no_grad():
+            for idx, (mixture, sources, assignment, threshold_weight) in enumerate(self.valid_loader):
+                """
+                mixture (batch_size, 1, 2*F_bin, T_bin)
+                sources (batch_size, n_sources, 2*F_bin, T_bin)
+                assignment (batch_size, n_sources, F_bin, T_bin)
+                threshold_weight (batch_size, F_bin, T_bin)
+                """
+                if self.use_cuda:
+                    mixture = mixture.cuda()
+                    sources = sources.cuda()
+                    threshold_weight = threshold_weight.cuda()
+                    assignment = assignment.cuda()
+                
+                real, imag = mixture[:,:,:F_bin], mixture[:,:,F_bin:]
+                mixture_amplitude = torch.sqrt(real**2+imag**2)
+                real, imag = sources[:,:,:F_bin], sources[:,:,F_bin:]
+                sources_amplitude = torch.sqrt(real**2+imag**2)
+                
+                output = self.model(mixture_amplitude, assignment=None, threshold_weight=threshold_weight, n_sources=n_sources)
+                # At the test phase, assignment may be unknown.
+                loss, _ = pit(self.criterion, output, sources_amplitude, batch_mean=False)
+                loss = loss.sum(dim=0)
+                valid_loss += loss.item()
+                
+                if idx < 5:
+                    mixture = mixture[0].cpu() # -> (1, 2*F_bin, T_bin)
+                    mixture_amplitude = mixture_amplitude[0].cpu() # -> (1, F_bin, T_bin)
+                    estimated_sources_amplitude = output[0].cpu() # -> (n_sources, F_bin, T_bin)
+                    ratio = estimated_sources_amplitude / mixture_amplitude
+                    real, imag = mixture[:,:F_bin], mixture[:,F_bin:]
+                    real, imag = ratio * real, ratio * imag
+                    estimated_sources = torch.cat([real, imag], dim=1) # -> (n_sources, 2*F_bin, T_bin)
+                    estimated_sources = self.istft(estimated_sources) # -> (n_sources, T)
+                    estimated_sources = estimated_sources.cpu().numpy()
+                    # TODO: .detach().cpu() is unnecessary?
+                    
+                    mixture = self.istft(mixture) # -> (1, T)
+                    mixture = mixture.squeeze(dim=0).numpy() # -> (T,)
+                    
+                    save_dir = os.path.join(self.sample_dir, "{}".format(idx+1))
+                    os.makedirs(save_dir, exist_ok=True)
+                    save_path = os.path.join(save_dir, "mixture.wav")
+                    norm = np.abs(mixture).max()
+                    mixture = mixture / norm
+                    write_wav(save_path, signal=mixture, sr=self.sr)
+                    
+                    for source_idx, estimated_source in enumerate(estimated_sources):
+                        save_path = os.path.join(save_dir, "epoch{}-{}.wav".format(epoch+1,source_idx))
+                        norm = np.abs(estimated_source).max()
+                        estimated_source = estimated_source / norm
+                        write_wav(save_path, signal=estimated_source, sr=self.sr)
+        
+        valid_loss /= n_valid
+        
+        return valid_loss
+        
+        
