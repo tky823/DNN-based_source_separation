822f970a56bf2c870249513410dc5ccc5bb5fb1b
tky1117 <delta9guitar97@gmail.com>
2020-09-14 02:02:38 +0900

    Remove unnecessary options

--- egs/librispeech/adanet/train.sh
+++ egs/librispeech/adanet/train.sh
@@ -16,7 +16,6 @@ sr=16000
 window_fn='hamming'
 fft_size=256
 hop_size=64
-ideal_mask='ibm'
 threshold=40
 
 # Model configuration
@@ -42,7 +41,7 @@ use_cuda=1
 overwrite=0
 seed=111
 
-save_dir="${exp_dir}/${n_sources}mix/${criterion}/stft${fft_size}-${hop_size}_${window_fn}-window_${ideal_mask}_threshold${threshold}/K${K}_H${H}_B${B}_N${N}_causal${causal}_mask-${mask_nonlinear}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-decay${weight_decay}/seed${seed}"
+save_dir="${exp_dir}/${n_sources}mix/${criterion}/stft${fft_size}-${hop_size}_${window_fn}-window_threshold${threshold}/K${K}_H${H}_B${B}_N${N}_causal${causal}_mask-${mask_nonlinear}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-decay${weight_decay}/seed${seed}"
 
 model_dir="${save_dir}/model"
 loss_dir="${save_dir}/loss"
@@ -63,7 +62,6 @@ train.py \
 --valid_json_path ${valid_json_path} \
 --sr ${sr} \
 --window_fn ${window_fn} \
---ideal_mask ${ideal_mask} \
 --threshold ${threshold} \
 --fft_size ${fft_size} \
 --hop_size ${hop_size} \
