3fb5109f3ef369be9fbd6dbdbdf76e92a024f07f
tky823 <40362510+tky823@users.noreply.github.com>
2020-12-28 01:27:41 +0900

    Rename output directory

--- src/algorithm/clustering.py
+++ src/algorithm/clustering.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     import pandas as pd
     import matplotlib.pyplot as plt
 
-    os.makedirs("data/clustering", exist_ok=True)
+    os.makedirs("data/Kmeans", exist_ok=True)
     random.seed(111)
 
     K = 2
@@ -76,7 +76,7 @@ if __name__ == '__main__':
 
     plt.figure()
     plt.scatter(data[:,0], data[:,1])
-    plt.savefig("data/clustering/faithful-0.png", bbox_inches='tight')
+    plt.savefig("data/Kmeans/faithful-0.png", bbox_inches='tight')
 
     kmeans = Kmeans(data, K=K)
     onehot_labels, centroids = kmeans(iteration=iteration) # (N, K), (K, D)
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     plt.figure()
     plt.scatter(data[:,0], data[:,1])
     plt.scatter(centroids[:,0], centroids[:,1], c='red')
-    plt.savefig("data/clustering/faithful-last.png", bbox_inches='tight')
+    plt.savefig("data/Kmeans/faithful-last.png", bbox_inches='tight')
 
     # or same as ...
 
@@ -96,5 +96,5 @@ if __name__ == '__main__':
         plt.figure()
         plt.scatter(data[:,0], data[:,1])
         plt.scatter(centroids[:,0], centroids[:,1], c='red')
-        plt.savefig("data/clustering/faithful-{}.png".format(idx+1), bbox_inches='tight')
+        plt.savefig("data/Kmeans/faithful-{}.png".format(idx+1), bbox_inches='tight')
 
