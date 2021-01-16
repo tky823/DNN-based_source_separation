e48f5dc72124899a05af790125dbcb33bfcf2172
tky823 <40362510+tky823@users.noreply.github.com>
2021-01-17 01:02:10 +0900

    bug fixes

--- egs/wsj0-mix/orpit_conv-tasnet/src/adhoc_driver.py
+++ egs/wsj0-mix/orpit_conv-tasnet/src/adhoc_driver.py
@@ -60,7 +60,7 @@ class ORPITTrainer(TrainerBase):
             train_loss = self.run_one_epoch(epoch)
             end = time.time()
             
-            print("[Epoch {}/{}] loss (train): {:.5f}, loss (valid): {:.5f}, {:.3f} [sec]".format(epoch+1, self.epochs, train_loss, valid_loss, end - start), flush=True)
+            print("[Epoch {}/{}] loss (train): {:.5f}, {:.3f} [sec]".format(epoch+1, self.epochs, train_loss, end - start), flush=True)
             
             self.train_loss[epoch] = train_loss
             
@@ -68,7 +68,7 @@ class ORPITTrainer(TrainerBase):
             self.save_model(epoch, model_path)
             
             save_path = os.path.join(self.loss_dir, "loss.png")
-            draw_loss_curve(train_loss=self.train_loss[:epoch+1], valid_loss=self.valid_loss[:epoch+1], save_path=save_path)
+            draw_loss_curve(train_loss=self.train_loss[:epoch+1], save_path=save_path)
     
     def run_one_epoch(self, epoch):
         """
