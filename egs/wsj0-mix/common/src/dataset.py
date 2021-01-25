701df010a4ef3c3f432dec944c7445bfa0e944b3
tky823 <40362510+tky823@users.noreply.github.com>
2021-01-25 20:57:33 +0900

    bug fixes

--- egs/wsj0-mix/common/src/dataset.py
+++ egs/wsj0-mix/common/src/dataset.py
@@ -406,43 +406,8 @@ def mixed_number_sources_train_collate_fn(batch):
     
     return batched_mixture, batched_sources
 
-"""
-def mixed_number_sources_train_collate_fn(batch):
-    batched_mixture, batched_sources = None, None
-    batched_n_sources = []
-    max_n_sources = 0
-
-    for mixture, sources, n_sources in batch:
-        mixture = mixture.unsqueeze(dim=0)
-        sources = sources.unsqueeze(dim=0)
-        
-        if batched_mixture is None:
-            max_n_sources = n_sources
-            batched_mixture = mixture
-            batched_sources = sources
-        else:
-            if n_sources > max_n_sources:
-                padding_size = list(batched_sources.size()) # (1, n_sources, *)
-                padding_size[1] = n_sources - max_n_sources
-                padding = torch.zeros(padding_size, dtype=torch.float) # (1, n_sources - max_n_sources, *)
-                batched_sources = torch.cat([batched_sources, padding], dim=1) # (1, n_sources, *)
-                max_n_sources = n_sources
-            elif n_sources < max_n_sources:
-                padding_size = list(sources.size()) # (1, n_sources, *)
-                padding_size[1] = max_n_sources - n_sources
-                padding = torch.zeros(padding_size, dtype=torch.float) # (1, max_n_sources - n_sources, *)
-                sources = torch.cat([sources, padding], dim=1) # (1, n_sources, *)
-
-            batched_mixture = torch.cat([batched_mixture, mixture], dim=0)
-            batched_sources = torch.cat([batched_sources, sources], dim=0)
-        
-        batched_n_sources.append(n_sources)
-    
-    return batched_mixture, batched_sources, batched_n_sources
-"""
-
 def mixed_number_sources_eval_collate_fn(batch):
-    batched_mixture, batched_sources, segment_ID = [], []
+    batched_mixture, batched_sources, segment_ID = [], [], []
     batched_segment_ID = []
 
     for mixture, sources in batch:
