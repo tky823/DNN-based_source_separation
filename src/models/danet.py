3f5debfa7e5487b5bf945c8cc606aa50b2ea3b5d
tky823 <40362510+tky823@users.noreply.github.com>
2021-01-29 11:49:38 +0900

    chenge the modules

--- src/models/danet.py
+++ src/models/danet.py
@@ -6,18 +6,27 @@ from algorithm.clustering import Kmeans
 EPS=1e-12
 
 class DANet(nn.Module):
-    def __init__(self, F_bin, embed_dim=20, hidden_channels=600, num_blocks=4, causal=False, mask_nonlinear='sigmoid', iter_clustering=10, eps=EPS, **kwargs):
+    def __init__(self, n_bins, embed_dim=20, hidden_channels=600, num_blocks=4, causal=False, mask_nonlinear='sigmoid', iter_clustering=10, eps=EPS, **kwargs):
         super().__init__()
         
-        self.F_bin = F_bin
+        self.n_bins = n_bins
         self.hidden_channels, self.embed_dim = hidden_channels, embed_dim
         self.num_blocks = num_blocks
-        
+
         self.causal = causal
+
+        if causal:
+            num_directions = 1
+            bidirectional = False
+        else:
+            num_directions = 2
+            bidirectional = True
+        
         self.mask_nonlinear = mask_nonlinear
         
-        self.lstm = StackedLSTM(F_bin, hidden_channels=hidden_channels, num_blocks=num_blocks, causal=causal)
-        self.fc = nn.Linear(hidden_channels, F_bin*embed_dim)
+        # self.lstm = StackedLSTM(n_bins, hidden_channels=hidden_channels, num_blocks=num_blocks, causal=causal)
+        self.lstm = nn.LSTM(n_bins, hidden_channels, num_layers=num_blocks, batch_first=True, bidirectional=bidirectional)
+        self.fc = nn.Linear(num_directions*hidden_channels, n_bins*embed_dim)
         
         if mask_nonlinear == 'sigmoid':
             self.mask_nonlinear2d = nn.Sigmoid()
@@ -34,11 +43,11 @@ class DANet(nn.Module):
     def forward(self, input, assignment=None, threshold_weight=None, n_sources=None, iter_clustering=None):
         """
         Args:
-            input (batch_size, 1, F_bin, T_bin): Amplitude
-            assignment (batch_size, n_sources, F_bin, T_bin): Speaker assignment when training
-            threshold_weight (batch_size, 1, F_bin, T_bin) or <float>
+            input (batch_size, 1, n_bins, T_bin): Amplitude
+            assignment (batch_size, n_sources, n_bins, T_bin): Speaker assignment when training
+            threshold_weight (batch_size, 1, n_bins, T_bin) or <float>
         Returns:
-            output (batch_size, n_sources, F_bin, T_bin)
+            output (batch_size, n_sources, n_bins, T_bin)
         """
         output, _ = self.extract_latent(input, assignment, threshold_weight=threshold_weight, n_sources=n_sources, iter_clustering=None)
         
@@ -47,9 +56,9 @@ class DANet(nn.Module):
     def extract_latent(self, input, assignment=None, threshold_weight=None, n_sources=None, iter_clustering=None):
         """
         Args:
-            input (batch_size, 1, F_bin, T_bin) <torch.Tensor>
-            assignment (batch_size, n_sources, F_bin, T_bin) <torch.Tensor>
-            threshold_weight (batch_size, 1, F_bin, T_bin) or <float>
+            input (batch_size, 1, n_bins, T_bin) <torch.Tensor>
+            assignment (batch_size, n_sources, n_bins, T_bin) <torch.Tensor>
+            threshold_weight (batch_size, 1, n_bins, T_bin) or <float>
         """
         if iter_clustering is None:
             iter_clustering = self.iter_clustering
@@ -65,40 +74,41 @@ class DANet(nn.Module):
         embed_dim = self.embed_dim
         eps = self.eps
         
-        batch_size, _, F_bin, T_bin = input.size()
+        batch_size, _, n_bins, T_bin = input.size()
         
         log_amplitude = torch.log(input + eps)
-        x = self.lstm(log_amplitude) # -> (batch_size, T_bin, F_bin)
-        x = self.fc(x) # -> (batch_size, T_bin, embed_dim*F_bin)
-        x = x.view(batch_size, T_bin, embed_dim, F_bin)
-        x = x.permute(0,2,3,1).contiguous()  # -> (batch_size, embed_dim, F_bin, T_bin)
-        latent = x.view(batch_size, embed_dim, F_bin*T_bin)
+        x = log_amplitude.squeeze(dim=1).permute(0,2,1) # -> (batch_size, T_bin, n_bins)
+        x, (_, _) = self.lstm(x) # -> (batch_size, T_bin, n_bins)
+        x = self.fc(x) # -> (batch_size, T_bin, embed_dim*n_bins)
+        x = x.view(batch_size, T_bin, embed_dim, n_bins)
+        x = x.permute(0,2,3,1).contiguous()  # -> (batch_size, embed_dim, n_bins, T_bin)
+        latent = x.view(batch_size, embed_dim, n_bins*T_bin)
         
         if assignment is None:
             # TODO: test threshold
             if self.training:
                 raise ValueError("assignment is required.")
-            latent_kmeans = latent.squeeze(dim=0) # -> (embed_dim, F_bin*T_bin)
-            latent_kmeans = latent_kmeans.permute(1,0) # -> (F_bin*T_bin, embed_dim)
+            latent_kmeans = latent.squeeze(dim=0) # -> (embed_dim, n_bins*T_bin)
+            latent_kmeans = latent_kmeans.permute(1,0) # -> (n_bins*T_bin, embed_dim)
             kmeans = Kmeans(latent_kmeans, K=n_sources)
-            _, centroids = kmeans(iteration=iter_clustering) # (F_bin*T_bin, n_sources), (n_sources, embed_dim)
+            _, centroids = kmeans(iteration=iter_clustering) # (n_bins*T_bin, n_sources), (n_sources, embed_dim)
             attractor = centroids.unsqueeze(dim=0) # (batch_size, n_sources, embed_dim)
         else:
-            threshold_weight = threshold_weight.view(batch_size, 1, F_bin*T_bin)
-            assignment = assignment.view(batch_size, n_sources, F_bin*T_bin) # -> (batch_size, n_sources, F_bin*T_bin)
+            threshold_weight = threshold_weight.view(batch_size, 1, n_bins*T_bin)
+            assignment = assignment.view(batch_size, n_sources, n_bins*T_bin) # -> (batch_size, n_sources, n_bins*T_bin)
             assignment = threshold_weight * assignment
             attractor = torch.bmm(assignment, latent.permute(0,2,1)) / (assignment.sum(dim=2, keepdim=True) + eps) # -> (batch_size, n_sources, embed_dim)
         
-        similarity = torch.bmm(attractor, latent) # -> (batch_size, n_sources, F_bin*T_bin)
-        similarity = similarity.view(batch_size, n_sources, F_bin, T_bin)
-        mask = self.mask_nonlinear2d(similarity) # -> (batch_size, n_sources, F_bin, T_bin)
+        similarity = torch.bmm(attractor, latent) # -> (batch_size, n_sources, n_bins*T_bin)
+        similarity = similarity.view(batch_size, n_sources, n_bins, T_bin)
+        mask = self.mask_nonlinear2d(similarity) # -> (batch_size, n_sources, n_bins, T_bin)
         output = mask * input
         
         return output, latent
     
     def get_package(self):
         package = {
-            'F_bin': self.F_bin,
+            'n_bins': self.n_bins,
             'embed_dim': self.embed_dim,
             'hidden_channels': self.hidden_channels,
             'num_blocks': self.num_blocks,
@@ -113,7 +123,7 @@ class DANet(nn.Module):
     def build_model(cls, model_path):
         package = torch.load(model_path, map_location=lambda storage, loc: storage)
         
-        F_bin = package['F_bin']
+        n_bins = package['n_bins']
         embed_dim = package['embed_dim']
         hidden_channels = package['hidden_channels']
         num_blocks = package['num_blocks']
@@ -123,7 +133,7 @@ class DANet(nn.Module):
         
         eps = package['eps']
         
-        model = cls(F_bin, embed_dim=embed_dim, hidden_channels=hidden_channels, num_blocks=num_blocks, causal=causal, mask_nonlinear=mask_nonlinear, eps=eps)
+        model = cls(n_bins, embed_dim=embed_dim, hidden_channels=hidden_channels, num_blocks=num_blocks, causal=causal, mask_nonlinear=mask_nonlinear, eps=eps)
         
         return model
     
@@ -136,73 +146,67 @@ class DANet(nn.Module):
                 
         return num_parameters
 
-class StackedLSTM(nn.Module):
-    def __init__(self, F_bin, hidden_channels=600, num_blocks=4, causal=False):
-        super().__init__()
-        
-        net = []
-        
-        self.num_blocks = num_blocks
-        
-        if causal:
-            bidirectional = False
-            num_directions = 1
-        else:
-            bidirectional = True
-            num_directions = 2
-        
-        for idx in range(num_blocks):
-            if idx == 0:
-                in_channels = F_bin
-                out_channels = hidden_channels//num_directions
-            else:
-                in_channels = hidden_channels
-                out_channels = hidden_channels//num_directions
-            
-            net.append(nn.LSTM(in_channels, out_channels, batch_first=True, bidirectional=bidirectional))
-        
-        self.net = nn.Sequential(*net)
-        
-    def forward(self, input):
-        """
-        Args:
-            input (batch_size, 1, F_bin, T_bin)
-        Returns:
-            output (batch_size, T_bin, F_bin)
-        """
-        x = input.squeeze(dim=1).permute(0,2,1) # -> (batch_size, T_bin, F_bin)
-        
-        for idx in range(self.num_blocks):
-            # To avoid error
-            x, (_, _) = self.net[idx](x)
-        
-        output = x
-
-        return output
-        
-if __name__ == '__main__':
-    from algorithm.ideal_mask import ideal_binary_mask
-    
+def _test_danet():
     torch.manual_seed(111)
-    
+
     batch_size = 2
     K = 10
     
     H = 32
     B = 4
     
-    F_bin, T_bin = 4, 128
+    n_bins, T_bin = 4, 128
     C = 2
     causal = False
     mask_nonlinear = 'sigmoid'
     
-    sources = torch.randint(0, 10, (batch_size, C, F_bin, T_bin), dtype=torch.float)
+    sources = torch.randn((batch_size, C, n_bins, T_bin), dtype=torch.float)
     input = sources.sum(dim=1, keepdim=True)
     assignment = ideal_binary_mask(sources)
-    threshold_weight = torch.randint(0, 2, (batch_size, 1, F_bin, T_bin), dtype=torch.float)
+    threshold_weight = torch.randint(0, 2, (batch_size, 1, n_bins, T_bin), dtype=torch.float)
     
-    model = DANet(F_bin, embed_dim=K, hidden_channels=H, num_blocks=B, causal=causal, mask_nonlinear=mask_nonlinear, n_sources=C)
+    model = DANet(n_bins, embed_dim=K, hidden_channels=H, num_blocks=B, causal=causal, mask_nonlinear=mask_nonlinear, n_sources=C)
     print(model)
+    print("# Parameters: {}".format(model.num_parameters))
+
     output = model(input, assignment, threshold_weight=threshold_weight)
     
     print(input.size(), output.size())
+
+def _test_danet_paper():
+    torch.manual_seed(111)
+
+    batch_size = 2
+    K = 20
+    
+    H = 600
+    B = 4
+    
+    n_bins, T_bin = 4, 128
+    C = 2
+    causal = False
+    mask_nonlinear = 'sigmoid'
+    
+    sources = torch.randn((batch_size, C, n_bins, T_bin), dtype=torch.float)
+    input = sources.sum(dim=1, keepdim=True)
+    assignment = ideal_binary_mask(sources)
+    threshold_weight = torch.randint(0, 2, (batch_size, 1, n_bins, T_bin), dtype=torch.float)
+    
+    model = DANet(n_bins, embed_dim=K, hidden_channels=H, num_blocks=B, causal=causal, mask_nonlinear=mask_nonlinear, n_sources=C)
+    print(model)
+    print("# Parameters: {}".format(model.num_parameters))
+
+    output = model(input, assignment, threshold_weight=threshold_weight)
+    
+    print(input.size(), output.size())
+        
+if __name__ == '__main__':
+    from algorithm.frequency_mask import ideal_binary_mask
+
+    print("="*10, "DANet", "="*10)
+    _test_danet()
+    print()
+
+    print("="*10, "DANet (same configuration in paper)", "="*10)
+    _test_danet_paper()
+    print()
