08ed4eac522e740d5bb22e99fb4f60bd3f8aadf1
tky823 <40362510+tky823@users.noreply.github.com>
2021-03-12 20:24:17 +0900

    [WIP] add modules of D3Net

--- src/models/d3net.py
+++ src/models/d3net.py
@@ -1,5 +1,4 @@
 import torch
-from torch._C import Value
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -13,41 +12,94 @@ See https://arxiv.org/abs/2010.01733
 EPS=1e-12
 
 class D3Net(nn.Module):
-    def __init__(self, eps=EPS, **kwargs):
+    def __init__(self, in_channels, growth_rate, kernel_size, num_d3blocks=4, num_d2blocks=3, depth=None, eps=EPS, **kwargs):
         super().__init__()
+
+        if type(growth_rate) is int:
+            growth_rate = [
+                growth_rate for _ in range(num_d3blocks)
+            ]
+        elif type(growth_rate) is list:
+            pass
+        else:
+            raise ValueError("Not support `growth_rate`={}".format(growth_rate))
+
+        if type(num_d2blocks) is int:
+            num_d2blocks = [
+                num_d2blocks for _ in range(num_d3blocks)
+            ]
+        elif type(num_d2blocks) is list:
+            pass
+        else:
+            raise ValueError("Not support `num_d2blocks`={}".format(num_d2blocks))
+
+        if depth is None:
+            depth = [
+                None for _ in range(num_d3blocks)
+            ]
+        elif type(depth) is int:
+            depth = [
+                depth for _ in range(num_d3blocks)
+            ]
+
+        net = []
+
+        for idx in range(num_d3blocks):
+            net.append(D3Block(in_channels, growth_rate[idx], kernel_size, num_blocks=num_d2blocks[idx], depth=depth[idx], eps=eps))
+            in_channels += growth_rate[idx] * num_d2blocks[idx] * depth[idx]
+        
+        self.net = nn.Sequential(*net)
+
+        self.num_d3blocks = num_d3blocks
 
         self.eps = eps
 
-        raise NotImplementedError("Implement D3Net")
     
     def forward(self, input):
-        raise NotImplementedError("Implement D3Net")
+        x = input
+        stacked = []
+
+        stacked.append(input)
+
+        for idx in range(self.num_d3blocks):
+            if idx != 0:
+                x = torch.cat(stacked, dim=1)
+            x = self.net[idx](x)
+            stacked.append(x)
+        
+        output = torch.cat(stacked[1:], dim=1)
+
+        return output
 
 class D3Block(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, num_blocks=3, depth=None, eps=EPS):
+    def __init__(self, in_channels, growth_rate, kernel_size, num_blocks=3, depth=None, eps=EPS):
         super().__init__()
 
         self.num_blocks = num_blocks
-        if type(out_channels) is int:
-            out_channels = [
-                out_channels for _ in range(num_blocks)
+
+        if type(growth_rate) is int:
+            growth_rate = [
+                growth_rate for _ in range(num_blocks)
             ]
-            if depth is None or type(depth) is int:
-                depth = [
-                    None for _ in range(num_blocks)
-                ]
-        elif type(out_channels) is list:
-            if depth is None or type(depth) is int:
-                depth = [
-                    None for _ in range(num_blocks)
-                ]
+        elif type(growth_rate) is list:
+            pass
         else:
-            raise ValueError("Not support `out_channels`={}".format(out_channels))
+            raise ValueError("Not support `growth_rate`={}".format(growth_rate))
+            
+        if depth is None:
+            depth = [
+                None for _ in range(num_blocks)
+            ]
+        elif type(depth) is int:
+            depth = [
+                depth for _ in range(num_blocks)
+            ]
 
         net = []
+
         for idx in range(num_blocks):
-            net.append(D2Block(in_channels, out_channels, kernel_size, depth=depth[idx], eps=eps))
-            in_channels += out_channels[-1]
+            net.append(D2Block(in_channels, growth_rate[idx], kernel_size, depth=depth[idx], eps=eps))
+            in_channels += growth_rate[idx] * depth[idx]
         
         self.net = nn.Sequential(*net)
 
@@ -60,32 +112,35 @@ class D3Block(nn.Module):
             output (batch_size, out_channels, n_bins, n_frames)
         """
         x = input
-        stacked = None
+        stacked = []
+
+        stacked.append(input)
 
         for idx in range(self.num_blocks):
-            if stacked is None:
-                stacked = x
-            else:
-                stacked = torch.cat([stacked, x], dim=1)
-            x = self.net[idx](stacked)
-            
-        output = x
+            if idx != 0:
+                x = torch.cat(stacked, dim=1)
+            x = self.net[idx](x)
+            stacked.append(x)
+        
+        output = torch.cat(stacked[1:], dim=1)
 
         return output
 
 class D2Block(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, depth=None, eps=EPS):
+    def __init__(self, in_channels, growth_rate, kernel_size, depth=None, eps=EPS):
         super().__init__()
 
-        if type(out_channels) is int:
+        if type(growth_rate) is int:
             assert depth is not None, "Specify `depth`"
-            out_channels = [
-                out_channels for _ in range(depth)
+            growth_rate = [
+                growth_rate for _ in range(depth)
             ]
-        elif type(out_channels) is list:
-            depth = len(out_channels)
+        elif type(growth_rate) is list:
+            if depth is not None:
+                assert depth == len(growth_rate), "`depth` is different from `len(growth_rate)`"
+            depth = len(growth_rate)
         else:
-            raise ValueError("Not support out_channels={}".format(out_channels))
+            raise ValueError("Not support growth_rate={}".format(growth_rate))
         self.depth = depth
 
         net = []
@@ -95,8 +150,8 @@ class D2Block(nn.Module):
             if idx == 0:
                 num_features.append(in_channels)
             else:
-                num_features.append(out_channels[idx-1])
-            net.append(MultiDilatedConv2d(num_features, out_channels[idx], kernel_size=kernel_size))
+                num_features.append(growth_rate[idx-1])
+            net.append(MultiDilatedConv2d(num_features, growth_rate[idx], kernel_size=kernel_size))
         self.net = nn.Sequential(*net)
 
         self.eps = eps
@@ -108,16 +163,18 @@ class D2Block(nn.Module):
             output (batch_size, out_channels, n_bins, n_frames)
         """
         x = input
-        stacked = None
+        stacked = []
+        output = []
+
+        stacked.append(input)
 
         for idx in range(self.depth):
-            if stacked is None:
-                stacked = x
-            else:
-                stacked = torch.cat([stacked, x], dim=1)
-            x = self.net[idx](stacked)
+            if idx != 0:
+                x = torch.cat(stacked, dim=1)
+            x = self.net[idx](x)
+            stacked.append(x)
         
-        output = x
+        output = torch.cat(stacked[1:], dim=1)
 
         return output
 
@@ -138,10 +195,10 @@ def _test_d2block():
     
     batch_size = 4
     H, W = 16, 32
-    in_channels, out_channels = 3, [2, 4, 6]
+    in_channels, groth_rate = 4, 2
 
     input = torch.randn(batch_size, in_channels, H, W)
-    model = D2Block(in_channels, out_channels, kernel_size=(3,3))
+    model = D2Block(in_channels, groth_rate, kernel_size=(3,3), depth=4)
     print(model)
     output = model(input)
     print(input.size(), output.size())
@@ -151,12 +208,29 @@ def _test_d3block():
     
     batch_size = 4
     H, W = 16, 32
-    in_channels, out_channels = 3, [2, 4, 6]
+    in_channels, growth_rate = 3, [2, 4, 6]
+    depth = 4
     num_blocks = 3
 
     input = torch.randn(batch_size, in_channels, H, W)
 
-    model = D3Block(in_channels, out_channels, kernel_size=(3,3), num_blocks=num_blocks)
+    model = D3Block(in_channels, growth_rate, kernel_size=(3,3), num_blocks=num_blocks, depth=depth)
+    print(model)
+    output = model(input)
+    print(input.size(), output.size())
+
+def _test_d3net():
+    torch.manual_seed(111)
+    
+    batch_size = 4
+    H, W = 16, 32
+    in_channels, growth_rate = 3, [2, 4, 6]
+    depth = [4, 5, 6]
+    num_d3blocks, num_d2blocks = 3, [2, 3, 4]
+
+    input = torch.randn(batch_size, in_channels, H, W)
+
+    model = D3Net(in_channels, growth_rate, kernel_size=(3,3), num_d3blocks=num_d3blocks, num_d2blocks=num_d2blocks, depth=depth)
     print(model)
     output = model(input)
     print(input.size(), output.size())
@@ -165,4 +239,6 @@ def _test_d3block():
 
 if __name__ == '__main__':
     # _test_d2block()
-    _test_d3block()
+    # _test_d3block()
+
+    _test_d3net()
