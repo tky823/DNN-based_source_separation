05ea486ee864c3fad26fa7d91b1725a24b0e127d
tky823 <40362510+tky823@users.noreply.github.com>
2021-03-14 01:34:50 +0900

    add codes for single target

---
+++ egs/musdb18/conv-tasnet/src/adhoc_dataset.py
@@ -0,0 +1,110 @@
+import os
+import numpy as np
+import musdb
+import torch
+
+__sources__=['drums','bass','other','vocals']
+
+EPS=1e-12
+
+class MUSDB18Dataset(torch.utils.data.Dataset):
+    def __init__(self, musdb18_root, sr=44100, target=None):
+        super().__init__()
+
+        assert target in __sources__, "target is unknown"
+        
+        self.musdb18_root = os.path.abspath(musdb18_root)
+        self.mus = musdb.DB(root=self.musdb18_root)
+
+        self.sr = sr
+        self.target = target
+
+class WaveDataset(MUSDB18Dataset):
+    def __init__(self, musdb18_root, sr=44100, target=None):
+        super().__init__(musdb18_root, sr=sr, target=target)
+
+        self.json_data = None
+
+    def __getitem__(self, idx):
+        """
+        Args:
+            idx <int>: index
+        Returns:
+            mixture (2, T) <torch.Tensor>
+            target (2, T) <torch.Tensor>
+            title <str>: title of song
+        """
+        data = self.json_data[idx]
+
+        songID = data['songID']
+        track = self.mus.tracks[songID]
+        title = track.title
+        track.sample_rate = self.sr
+        track.chunk_start = data['start']
+        track.chunk_duration = data['duration']
+
+        mixture = track.audio.transpose(1, 0)[None]
+        target = track.targets[self.target].audio.transpose(1, 0)
+        mixture = torch.Tensor(mixture).float()
+        target = torch.Tensor(target).float()
+
+        return mixture, target, title
+
+    def __len__(self):
+        return len(self.json_data)
+
+
+class WaveTrainDataset(WaveDataset):
+    def __init__(self, musdb18_root, sr=44100, duration=4, overlap=None, target=None):
+        super().__init__(musdb18_root, sr=sr, target=target)
+
+        self.mus = musdb.DB(root=self.musdb18_root, subsets="train", split='train')
+
+        self.duration = duration
+
+        if overlap is None:
+            overlap = self.duration / 2
+
+        self.json_data = []
+
+        for songID, track in enumerate(self.mus.tracks):
+            for start in np.arange(0, track.duration, duration - overlap):
+                if start + duration >= track.duration:
+                    break
+                data = {
+                    'songID': songID,
+                    'start': start,
+                    'duration': duration
+                }
+                self.json_data.append(data)
+    
+    def __getitem__(self, idx):
+        mixture, sources, _ = super().__getitem__(idx)
+        
+        return mixture, sources
+
+class WaveEvalDataset(WaveDataset):
+    def __init__(self, musdb18_root, sr=44100, max_duration=4, target=None):
+        super().__init__(musdb18_root, sr=sr, target=target)
+
+        self.mus = musdb.DB(root=self.musdb18_root, subsets="train", split='valid')
+
+        self.max_duration = max_duration
+
+        self.json_data = []
+
+        for songID, track in enumerate(self.mus.tracks):
+            if max_duration is None:
+                duration = track.duration
+            else:
+                if track.duration < max_duration:
+                    duration = track.duration
+                else:
+                    duration = max_duration
+            
+            data = {
+                'songID': songID,
+                'start': 0,
+                'duration': duration
+            }
+            self.json_data.append(data)
