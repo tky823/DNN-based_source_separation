b85140e02fa40d9ed2895a0c40955f036c1f8098
tky823 <40362510+tky823@users.noreply.github.com>
2021-03-22 18:21:51 +0900

    bug fixes

--- egs/wsj0-mix/common/prepare_unknown_number_mixture.sh
+++ egs/wsj0-mix/common/prepare_unknown_number_mixture.sh
@@ -15,7 +15,7 @@ n_sources_set=`echo ${mixed_n_sources} | tr '+' '\n'`
 
 for n_sources in ${n_sources_set} ; do
     from="${mix_dir}/${n_sources}speakers/wav${sr_k}k/${minmax}/*"
-    cp "${from}" "${to_dir}"
+    cp -r "${from}" "${to_dir}"
 
     for data_type in 'tr' 'cv' 'tt' ; do
         cat "${mix_dir}/${n_sources}speakers/mix_${n_sources}_spk_${minmax}_${data_type}_mix" >> "${mix_dir}/${mixed_n_sources}speakers/mix_${mixed_n_sources}_spk_${minmax}_${data_type}_mix"
