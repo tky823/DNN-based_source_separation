63345f414b01fe2cf68aeae480084ad3edb9f12a
tky823 <40362510+tky823@users.noreply.github.com>
2021-06-02 22:24:04 +0900

    bug fixes

--- src/models/film.py
+++ src/models/film.py
@@ -1,36 +1,151 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class FiLM(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.gamma, self.beta = None, None
+    def forward(self, input, gamma, beta):
+        """
+        Args:
+            input (batch_size, num_features, *)
+            gamma (batch_size, num_features)
+            beta (batch_size, num_features)
+        Returns:
+            output (batch_size, num_features, *)
+        """
+        n_dims = input.dim()
+        expand_dims = (1,) * n_dims
+        dims = gamma.size() + expand_dims
 
-    def forward(self, input):
-        return self.gamma * input + self.beta
+        gamma = gamma.view(*dims)
+        beta = beta.view(*dims)
+        
+        return gamma * input + beta
 
 class FiLM1d(FiLM):
-    def __init__(self, num_features):
+    def __init__(self):
         super().__init__()
+    
+    def forward(self, input, gamma, beta):
+        """
+        Args:
+            input (batch_size, num_features, T)
+            gamma (batch_size, num_features)
+            beta (batch_size, num_features)
+        Returns:
+            output (batch_size, num_features, T)
+        """
+        dims = gamma.size() + (1,)
 
-        self.gamma = nn.Parameter(torch.Tensor(1, num_features, 1))
-        self.beta = nn.Parameter(torch.Tensor(1, num_features, 1))
+        gamma = gamma.view(*dims)
+        beta = beta.view(*dims)
+
+        return gamma * input + beta
 
 class FiLM2d(FiLM):
-    def __init__(self, num_features):
+    def __init__(self):
         super().__init__()
 
-        self.gamma = nn.Parameter(torch.Tensor(1, num_features, 1, 1))
-        self.beta = nn.Parameter(torch.Tensor(1, num_features, 1, 1))
+    def forward(self, input, gamma, beta):
+        """
+        Args:
+            input (batch_size, num_features, H, W)
+            gamma (batch_size, num_features)
+            beta (batch_size, num_features)
+        Returns:
+            output (batch_size, num_features, H, W)
+        """
+        dims = gamma.size() + (1, 1)
+
+        gamma = gamma.view(*dims)
+        beta = beta.view(*dims)
+        
+        return gamma * input + beta
 
 class FiLM3d(FiLM):
-    def __init__(self, num_features):
+    def __init__(self):
         super().__init__()
 
-        self.gamma = nn.Parameter(torch.Tensor(1, num_features, 1, 1, 1))
-        self.beta = nn.Parameter(torch.Tensor(1, num_features, 1, 1, 1))
+    def forward(self, input, gamma, beta):
+        """
+        Args:
+            input (batch_size, num_features, H, W, D)
+            gamma (batch_size, num_features)
+            beta (batch_size, num_features)
+        Returns:
+            output (batch_size, num_features, H, W, D)
+        """
+        dims = gamma.size() + (1, 1, 1)
+
+        gamma = gamma.view(*dims)
+        beta = beta.view(*dims)
+        
+        return gamma * input + beta
+
+def _test_film():
+    torch.manual_seed(111)
+
+    batch_size, in_channels = 4, 3
+
+    print("-"*10, "FiLM1d", "-"*10)
+
+    T = 5
+    input = torch.randn((batch_size, in_channels, T), dtype=torch.float)
+    gamma, beta = torch.randn((batch_size, in_channels), dtype=torch.float), torch.randn((batch_size, in_channels), dtype=torch.float)
+    model = FiLM1d()
+    output = model(input, gamma, beta)
+
+    print(model)
+    print(input.size(), output.size())
+    print()
+
+    print("-"*10, "FiLM2d", "-"*10)
+
+    H, W = 5, 6
+    input = torch.randn((batch_size, in_channels, H, W), dtype=torch.float)
+    gamma, beta = torch.randn((batch_size, in_channels), dtype=torch.float), torch.randn((batch_size, in_channels), dtype=torch.float)
+    model = FiLM2d()
+    output = model(input, gamma, beta)
+
+    print(model)
+    print(input.size(), output.size())
+    print()
+
+    print("-"*10, "FiLM3d", "-"*10)
+
+    H, W, D = 5, 6, 7
+    input = torch.randn((batch_size, in_channels, H, W, D), dtype=torch.float)
+    gamma, beta = torch.randn((batch_size, in_channels), dtype=torch.float), torch.randn((batch_size, in_channels), dtype=torch.float)
+    model = FiLM3d()
+    output = model(input, gamma, beta)
+
+    print(model)
+    print(input.size(), output.size())
+    print()
+
+    print("-"*10, "FiLM (any dimension is acceptable)", "-"*10)
+
+    T = 5
+    input = torch.randn((batch_size, in_channels, T), dtype=torch.float)
+    gamma, beta = torch.randn((batch_size, in_channels), dtype=torch.float), torch.randn((batch_size, in_channels), dtype=torch.float)
+    model = FiLM()
+    output = model(input, gamma, beta)
+
+    print(model)
+    print(input.size(), output.size())
+    print()
+
+    H, W, D = 5, 6, 7
+    input = torch.randn((batch_size, in_channels, H, W, D), dtype=torch.float)
+    gamma, beta = torch.randn((batch_size, in_channels), dtype=torch.float), torch.randn((batch_size, in_channels), dtype=torch.float)
+    model = FiLM()
+    output = model(input, gamma, beta)
+
+    print(model)
+    print(input.size(), output.size())
+    print()
 
 if __name__ == '__main__':
-    model = FiLM1d()
+    
+    _test_film()
