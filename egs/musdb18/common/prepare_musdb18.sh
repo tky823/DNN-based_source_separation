f3e257fcc7201e58fa0607580fe5284f1ee80232
tky823 <40362510+tky823@users.noreply.github.com>
2021-06-12 09:48:52 +0900

    add dataset downloader

---
+++ egs/musdb18/common/prepare_musdb18.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+musdb18_root="../../../dataset/musdb18"
+file=musdb18.zip
+
+if [ -e "${musdb18_root}/train" ]; then
+    echo "Already downloaded dataset ${musdb18_root}"
+else
+    mkdir -p "${musdb18_root}/train"
+    wget "https://zenodo.org/record/1117372/files/${file}" -P "/tmp"
+    # wget "https://zenodo.org/record/3338373/files/${file}" -P "/tmp"
+    unzip "${file}" -d "${musdb18_root}"
+    rm "/tmp/${file}"
+fi
+
+
