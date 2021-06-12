52fcdba2d6930d712f6fcda5b8c7496dfff3b7e1
tky823 <40362510+tky823@users.noreply.github.com>
2021-06-12 11:00:32 +0900

    add preparation script

--- egs/musdb18/common/prepare_musdb18.sh
+++ egs/musdb18/common/prepare_musdb18.sh
@@ -5,7 +5,7 @@ file=musdb18.zip
 
 . ./parse_options.sh || exit 1
 
-if [ -e "${musdb18_root}/train" ]; then
+if [ -e "${musdb18_root}/train/A Classic Education - NightOwl.stem.mp4" ]; then
     echo "Already downloaded dataset ${musdb18_root}"
 else
     mkdir -p "${musdb18_root}/train"
