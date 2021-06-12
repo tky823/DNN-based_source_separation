765654305318a02afc4d76d24c632372face4bfa
tky823 <40362510+tky823@users.noreply.github.com>
2021-06-12 12:48:00 +0900

    update dataset

--- egs/musdb18/common/prepare_musdb18.sh
+++ egs/musdb18/common/prepare_musdb18.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 musdb18_root="../../../dataset/musdb18"
+musdb18hq_root="../../../dataset/musdb18hq"
 file=musdb18.zip
+is_hq=0
 
 . ./parse_options.sh || exit 1
 
-if [ -e "${musdb18_root}/train/A Classic Education - NightOwl.stem.mp4" ]; then
-    echo "Already downloaded dataset ${musdb18_root}"
+if [ ${is_hq} -eq 0 ]; then
+    if [ -e "${musdb18_root}/train/A Classic Education - NightOwl.stem.mp4" ]; then
+        echo "Already downloaded dataset ${musdb18_root}"
+    else
+        mkdir -p "${musdb18_root}/train"
+        wget "https://zenodo.org/record/1117372/files/${file}" -P "/tmp"
+        unzip "${file}" -d "${musdb18_root}"
+        rm "/tmp/${file}"
+    fi
 else
-    mkdir -p "${musdb18_root}/train"
-    wget "https://zenodo.org/record/1117372/files/${file}" -P "/tmp"
-    # wget "https://zenodo.org/record/3338373/files/${file}" -P "/tmp"
-    unzip "${file}" -d "${musdb18_root}"
-    rm "/tmp/${file}"
+    if [ -e "${musdb18hq_root}/train/A Classic Education - NightOwl.stem.mp4" ]; then
+        echo "Already downloaded dataset ${musdb18hq_root}"
+    else
+        mkdir -p "${musdb18hq_root}/train"
+        wget "https://zenodo.org/record/3338373/files/${file}" -P "/tmp"
+        unzip "${file}" -d "${musdb18hq_root}"
+        rm "/tmp/${file}"
+    fi
 fi
-
-
