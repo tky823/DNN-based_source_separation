faf40ff6897cce7aae98ae0ca40883944d4972ed
tky823 <40362510+tky823@users.noreply.github.com>
2021-08-13 01:18:04 +0900

    add assertion

--- egs/musdb18/meta-tasnet/src/adhoc_driver.py
+++ egs/musdb18/meta-tasnet/src/adhoc_driver.py
@@ -39,6 +39,7 @@ class Trainer(TrainerBase):
                 mixture = mixture.cuda()
                 sources = sources.cuda()
             
+            print(sources.size())
             batch_size, n_sources, T = sources.size()
 
             mixture, sources = mixture.view(batch_size, T), sources.view(batch_size * n_sources, T)
