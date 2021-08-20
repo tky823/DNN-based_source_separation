ed473bec062e5266e3fce03966c538cef1f93137
tky823 <40362510+tky823@users.noreply.github.com>
2021-08-21 00:01:48 +0900

    update dataset

--- egs/musdb18/d3net/local/train.py
+++ egs/musdb18/d3net/local/train.py
@@ -48,10 +48,11 @@ def main(args):
     
     args.sources = args.sources.replace('[', '').replace(']', '').split(',')
     patch_samples = (args.hop_size * (args.patch_size - 1 - (args.fft_size - args.hop_size) // args.hop_size - 1) + args.fft_size)
+    max_samples = int(args.max_duration * args.sr)
     samples_per_epoch = None
     
     train_dataset = SpectrogramTrainDataset(args.musdb18_root, fft_size=args.fft_size, hop_size=args.hop_size, sr=args.sr, patch_samples=patch_samples, samples_per_epoch=samples_per_epoch, sources=args.sources, target=args.target, augmentation=True, is_wav=args.is_wav)
-    valid_dataset = SpectrogramEvalDataset(args.musdb18_root, fft_size=args.fft_size, hop_size=args.hop_size, sr=args.sr, patch_samples=patch_samples, max_duration=args.max_duration, sources=args.sources, target=args.target, is_wav=args.is_wav)
+    valid_dataset = SpectrogramEvalDataset(args.musdb18_root, fft_size=args.fft_size, hop_size=args.hop_size, sr=args.sr, patch_samples=patch_samples, max_samples=max_samples, sources=args.sources, target=args.target, is_wav=args.is_wav)
     
     print("Training dataset includes {} samples.".format(len(train_dataset)))
     print("Valid dataset includes {} samples.".format(len(valid_dataset)))
