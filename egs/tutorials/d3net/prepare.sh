97ab15600f3eb6f144573b459ed14fe720c22c13
tky823 <40362510+tky823@users.noreply.github.com>
2021-08-27 10:16:11 +0900

    add parse_options.sh for D3Net separation example

--- egs/tutorials/d3net/prepare.sh
+++ egs/tutorials/d3net/prepare.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-ID="1rzUBvqMyleVcWA-NNE5XN0BvX1VDaV4A"
+model_id="1GI1TEWsoMxlvVdKuLvP6aeau26fNy3Qb"
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${ID}" > /dev/null
+. ./path.sh
+. parse_options.sh || exit 1
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${model_id}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${ID}" -o "model.zip"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${model_id}" -o "model.zip"
 
 unzip "model.zip"
