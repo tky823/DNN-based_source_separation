1ef9c949c146f5a21aa04907f749a16d5e0015b9
tky823 <40362510+tky823@users.noreply.github.com>
2021-08-30 02:12:42 +0900

    bug fixes (tar)

--- egs/slakh2100/common/prepare_slakh2100.sh
+++ egs/slakh2100/common/prepare_slakh2100.sh
@@ -10,11 +10,11 @@ mkdir -p "${slakh2100_root}"
 if [ ${is_tiny} -eq 0 ]; then
     file="slakh2100_flac_redux.tar.gz"
     wget "https://zenodo.org/record/4599666/files/${file}" -P "/tmp"
-    unzip "/tmp/${file}" -d "${slakh2100_root}"
+    tar -zxvf "/tmp/${file}" -C "${slakh2100_root}"
     rm "/tmp/${file}"
 else
     file="babyslakh_16k.tar.gz"
     wget "https://zenodo.org/record/4603870/files/${file}" -P "/tmp"
-    unzip "/tmp/${file}" -d "${slakh2100_root}"
+    tar -zxvf "/tmp/${file}" -C "${slakh2100_root}"
     rm "/tmp/${file}"
 fi
