72e156d2e7fc951a43359648942631308b214ade
tky823 <40362510+tky823@users.noreply.github.com>
2021-08-30 02:18:24 +0900

    bug fixes

--- egs/slakh2100/common/prepare_slakh2100.sh
+++ egs/slakh2100/common/prepare_slakh2100.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-slakh2100_root="../../../dataset/slakh2100"
+dataset_root="../../../dataset"
 is_tiny=0
 
 . ./parse_options.sh || exit 1
 
-mkdir -p "${slakh2100_root}"
+mkdir -p "${dataset_root}"
 
 if [ ${is_tiny} -eq 0 ]; then
     file="slakh2100_flac_redux.tar.gz"
     wget "https://zenodo.org/record/4599666/files/${file}" -P "/tmp"
-    tar -zxvf "/tmp/${file}" -C "${slakh2100_root}"
+    tar -zxvf "/tmp/${file}" -C "${dataset_root}"
     rm "/tmp/${file}"
 else
     file="babyslakh_16k.tar.gz"
     wget "https://zenodo.org/record/4603870/files/${file}" -P "/tmp"
-    tar -zxvf "/tmp/${file}" -C "${slakh2100_root}"
+    tar -zxvf "/tmp/${file}" -C "${dataset_root}"
     rm "/tmp/${file}"
 fi
