7c6f947ecf1ccc2eddab64de8ebb05721030736b
tky823 <40362510+tky823@users.noreply.github.com>
2021-08-30 15:57:46 +0900

    update driver for OpenUnmix

---
+++ egs/musdb18/open-unmix/local/test.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import argparse
+
+import torch
+import torch.nn as nn
+
+from utils.utils import set_seed
+from adhoc_dataset import SpectrogramTestDataset, TestDataLoader
+from adhoc_driver import AdhocTester
+from models.umx import OpenUnmix, ParallelOpenUnmix
+from criterion.distance import MeanSquaredError
+
+parser = argparse.ArgumentParser(description="Evaluation of OpenUnmix")
+
+parser.add_argument('--musdb18_root', type=str, default=None, help='Path to MUSDB18')
+parser.add_argument('--sr', type=int, default=10, help='Sampling rate')
+parser.add_argument('--duration', type=float, default=6, help='Duration')
+parser.add_argument('--fft_size', type=int, default=4096, help='FFT length')
+parser.add_argument('--hop_size', type=int, default=1024, help='Hop length')
+parser.add_argument('--window_fn', type=str, default='hamming', help='Window function')
+parser.add_argument('--sources', type=str, default="[drums,bass,other,vocals]", help='Source names')
+parser.add_argument('--criterion', type=str, default='mse', choices=['mse'], help='Criterion')
+parser.add_argument('--model_dir', type=str, default=None, help='Directory which includes drums/<model_choice>.pth, ..., vocals/<model_choice>.pth')
+parser.add_argument('--estimates_dir', type=str, default=None, help='Estimated sources output directory')
+parser.add_argument('--json_dir', type=str, default=None, help='Json directory')
+parser.add_argument('--model_choice', type=str, default='last', choices=['best', 'last'], help='Model choice. Default: last')
+parser.add_argument('--use_norbert', type=int, default=0, help='Use norbert.wiener for multichannel wiener filetering. 0: Not use norbert, 1: Use norbert (you have to install norbert)')
+parser.add_argument('--use_cuda', type=int, default=1, help='0: Not use cuda, 1: Use cuda')
+parser.add_argument('--seed', type=int, default=42, help='Random seed')
+
+def main(args):
+    set_seed(args.seed)
+    
+    args.sources = args.sources.replace('[', '').replace(']', '').split(',')
+    samples = int(args.duration * args.sr)
+    test_dataset = SpectrogramTestDataset(args.musdb18_root, fft_size=args.fft_size, hop_size=args.hop_size, sr=args.sr, samples=samples, sources=args.sources, target=args.sources)
+    print("Test dataset includes {} samples.".format(len(test_dataset)))
+    
+    loader = TestDataLoader(test_dataset, batch_size=1, shuffle=False)
+    
+    modules = {}
+    for source in args.sources:
+        model_path = os.path.join(args.model_dir, source, "{}.pth".format(args.model_choice))
+        modules[source] = OpenUnmix.build_model(model_path)
+    
+    model = ParallelOpenUnmix(modules)
+    
+    print(model)
+    print("# Parameters: {}".format(model.num_parameters))
+    
+    if args.use_cuda:
+        if torch.cuda.is_available():
+            model.cuda()
+            model = nn.DataParallel(model)
+            print("Use CUDA")
+        else:
+            raise ValueError("Cannot use CUDA.")
+    else:
+        print("Does NOT use CUDA")
+    
+    # Criterion
+    if args.criterion == 'mse':
+        criterion = MeanSquaredError(dim=(1,2,3))
+    else:
+        raise ValueError("Not support criterion {}".format(args.criterion))
+    
+    tester = AdhocTester(model, loader, criterion, args)
+    tester.run()
+    
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)
+    main(args)
--- egs/musdb18/open-unmix/src/adhoc_driver.py
+++ egs/musdb18/open-unmix/src/adhoc_driver.py
@@ -1,12 +1,15 @@
 import os
 import time
+import math
 
+import musdb
+import museval
 import torch
 import torchaudio
 import torch.nn as nn
 
 from utils.utils import draw_loss_curve
-from driver import TrainerBase
+from driver import TrainerBase, TesterBase
 
 BITS_PER_SAMPLE_MUSDB18 = 16
 EPS = 1e-12
@@ -225,3 +228,340 @@ class AdhocTrainer(TrainerBase):
         package['epoch'] = epoch + 1
         
         torch.save(package, model_path)
+
+class AdhocTester(TesterBase):
+    def __init__(self, model, loader, criterion, args):
+        super().__init__(model, loader, criterion, args)
+
+    def _reset(self, args):
+        self.sr = args.sr
+        self.sources = args.sources
+
+        self.musdb18_root = args.musdb18_root
+
+        self.fft_size, self.hop_size = args.fft_size, args.hop_size    
+        self.window = self.loader.dataset.window
+        self.normalize = self.loader.dataset.normalize
+        
+        self.model_dir = args.model_dir
+        self.estimates_dir = args.estimates_dir
+        self.json_dir = args.json_dir
+        
+        if self.estimates_dir is not None:
+            self.estimates_dir = os.path.abspath(args.estimates_dir)
+            os.makedirs(self.estimates_dir, exist_ok=True)
+        
+        if self.json_dir is not None:
+            self.json_dir = os.path.abspath(args.json_dir)
+            os.makedirs(self.json_dir, exist_ok=True)
+        
+        self.use_cuda = args.use_cuda
+        self.use_norbert = args.use_norbert
+
+        is_data_parallel = isinstance(self.model, nn.DataParallel)
+        
+        for target in self.sources:
+            model_path = os.path.join(self.model_dir, target, "{}.pth".format(args.model_choice))
+            package = torch.load(model_path, map_location=lambda storage, loc: storage)
+            if is_data_parallel:
+                self.model.module.net[target].load_state_dict(package['state_dict'])
+            else:
+                self.model.net[target].load_state_dict(package['state_dict'])
+        
+        if self.use_norbert:
+            try:
+                import norbert
+            except:
+                raise ImportError("Cannot import norbert.")
+    
+    def run(self):
+        self.estimate_all()
+        self.eval_all()
+
+    def estimate_all(self):
+        self.model.eval()
+        
+        test_loss = 0
+        test_loss_improvement = 0
+        n_test = len(self.loader.dataset)
+        
+        with torch.no_grad():
+            for idx, (mixture, sources, samples, name) in enumerate(self.loader):
+                """
+                    mixture: (1, n_mics, n_bins, n_frames)
+                    sources: (n_sources, n_mics, n_bins, n_frames)
+                    samples <int>: Length in time domain
+                    name <str>: Artist and title of song
+                """
+                if self.use_cuda:
+                    mixture = mixture.cuda()
+                    sources = sources.cuda()
+                
+                mixture_amplitude = torch.abs(mixture)
+                sources_amplitude = torch.abs(sources)
+                
+                estimated_sources_amplitude = []
+
+                # Serial operation
+                for target in self.sources:
+                    _estimated_sources_amplitude = self.model(mixture_amplitude, target=target)
+                    estimated_sources_amplitude.append(_estimated_sources_amplitude)
+                
+                estimated_sources_amplitude = torch.cat(estimated_sources_amplitude, dim=0) # (n_sources, n_mics, n_bins, n_frames)
+                
+                loss_mixture = self.criterion(mixture_amplitude, sources_amplitude, batch_mean=False) # (n_sources,)
+                loss = self.criterion(estimated_sources_amplitude, sources_amplitude, batch_mean=False) # (n_sources,)
+                loss_improvement = loss_mixture - loss # (n_sources,)
+
+                mixture = mixture.cpu()
+                estimated_sources_amplitude = estimated_sources_amplitude.cpu()
+
+                estimated_sources = self.apply_multichannel_wiener_filter(mixture, estimated_sources_amplitude=estimated_sources_amplitude)
+                estimated_sources_channels = estimated_sources.size()[:-2]
+
+                estimated_sources = estimated_sources.view(-1, *estimated_sources.size()[-2:])
+                estimated_sources = torch.istft(estimated_sources, self.fft_size, hop_length=self.hop_size, window=self.window, normalized=self.normalize, return_complex=False)
+                estimated_sources = estimated_sources.view(*estimated_sources_channels, -1) # -> (n_sources, n_mics, T_pad)
+
+                track_dir = os.path.join(self.estimates_dir, name)
+                os.makedirs(track_dir, exist_ok=True)
+
+                for source_idx, target in enumerate(self.sources):
+                    estimated_path = os.path.join(track_dir, "{}.wav".format(target))
+                    estimated_source = estimated_sources[source_idx, :, :samples] # -> (n_mics, T)
+                    signal = estimated_source.unsqueeze(dim=0) if estimated_source.dim() == 1 else estimated_source
+                    torchaudio.save(estimated_path, signal, sample_rate=self.sr, bits_per_sample=BITS_PER_SAMPLE_MUSDB18)
+                
+                test_loss += loss # (n_sources,)
+                test_loss_improvement += loss_improvement # (n_sources,)
+
+        test_loss /= n_test
+        test_loss_improvement /= n_test
+        
+        s = "Loss:"
+        for idx, target in enumerate(self.sources):
+            s += " ({}) {:.3f}".format(target, test_loss[idx].item())
+        
+        s += ", loss improvement:"
+        for idx, target in enumerate(self.sources):
+            s += " ({}) {:.3f}".format(target, test_loss_improvement[idx].item())
+
+        print(s)
+    
+    def eval_all(self):
+        mus = musdb.DB(root=self.musdb18_root, subsets='test')
+        
+        results = museval.EvalStore(frames_agg='median', tracks_agg='median')
+
+        for track in mus.tracks:
+            name = track.name
+            
+            estimates = {}
+            estimated_accompaniment = 0
+
+            for target in self.sources:
+                estimated_path = os.path.join(self.estimates_dir, name, "{}.wav".format(target))
+                estimated, _ = torchaudio.load(estimated_path)
+                estimated = estimated.numpy().transpose(1, 0)
+                estimates[target] = estimated
+                estimated_accompaniment += estimated
+
+            estimates['accompaniment'] = estimated_accompaniment
+
+            # Evaluate using museval
+            scores = museval.eval_mus_track(track, estimates, output_dir=self.json_dir)
+            results.add_track(scores)
+
+            print(name)
+            print(scores, flush=True)
+
+        print(results)
+
+    def apply_multichannel_wiener_filter(self, mixture, estimated_sources_amplitude, channels_first=True, eps=EPS):
+        if self.use_norbert:
+            estimated_sources = apply_multichannel_wiener_filter_norbert(mixture, estimated_sources_amplitude, channels_first=channels_first, eps=eps)
+        else:
+            estimated_sources = apply_multichannel_wiener_filter_torch(mixture, estimated_sources_amplitude, channels_first=channels_first, eps=eps)
+
+        return estimated_sources
+    
+def apply_multichannel_wiener_filter_norbert(mixture, estimated_sources_amplitude, iteration=1, channels_first=True, eps=EPS):
+    """
+    Args:
+        mixture <torch.Tensor>: (1, n_channels, n_bins, n_frames) or (n_channels, n_bins, n_frames), complex tensor
+        estimated_sources_amplitude <torch.Tensor>: (n_sources, n_channels, n_bins, n_frames), real (nonnegative) tensor
+    Returns:
+        estimated_sources <torch.Tensor>: (n_sources, n_channels, n_bins, n_frames), complex tensor
+    """
+    import norbert
+
+    assert channels_first, "`channels_first` is expected True, but given {}".format(channels_first)
+
+    n_dims = mixture.dim()
+
+    if n_dims == 4:
+        mixture = mixture.squeeze(dim=0)
+    elif n_dims != 3:
+        raise ValueError("mixture.dim() is expected 3 or 4, but given {}.".format(mixture.dim()))
+
+    assert estimated_sources_amplitude.dim() == 4, "estimated_sources_amplitude.dim() is expected 4, but given {}.".format(estimated_sources_amplitude.dim())
+
+    device = mixture.device
+    dtype = mixture.dtype
+
+    mixture = mixture.detach().cpu().numpy()
+    estimated_sources_amplitude = estimated_sources_amplitude.detach().cpu().numpy()
+
+    mixture = mixture.transpose(2, 1, 0)
+    estimated_sources_amplitude = estimated_sources_amplitude.transpose(3, 2, 1, 0)
+    estimated_sources = norbert.wiener(estimated_sources_amplitude, mixture, iterations=iteration, eps=eps)
+    estimated_sources = estimated_sources.transpose(3, 2, 1, 0)
+    estimated_sources = torch.from_numpy(estimated_sources).to(device, dtype)
+
+    return estimated_sources
+
+def apply_multichannel_wiener_filter_torch(mixture, estimated_sources_amplitude, iteration=1, channels_first=True, eps=EPS):
+    """
+    Multichannel Wiener filter.
+    Implementation is based on norbert package.
+    Args:
+        mixture <torch.Tensor>: Complex tensor with shape of (1, n_channels, n_bins, n_frames) or (n_channels, n_bins, n_frames) or (batch_size, 1, n_channels, n_bins, n_frames) or (batch_size, n_channels, n_bins, n_frames)
+        estimated_sources_amplitude <torch.Tensor>: (n_sources, n_channels, n_bins, n_frames) or (batch_size, n_sources, n_channels, n_bins, n_frames)
+        iteration <int>: Iteration of EM algorithm updates
+        channels_first <bool>: Only supports True
+        eps <float>: small value for numerical stability
+    """
+    assert channels_first, "`channels_first` is expected True, but given {}".format(channels_first)
+
+    n_dims = estimated_sources_amplitude.dim()
+    n_dims_mixture = mixture.dim()
+
+    if n_dims == 4:
+        """
+        Shape of mixture is (1, n_channels, n_bins, n_frames) or (n_channels, n_bins, n_frames)
+        """
+        if n_dims_mixture == 4:
+            mixture = mixture.squeeze(dim=1) # (n_channels, n_bins, n_frames)
+        elif n_dims_mixture != 3:
+            raise ValueError("mixture.dim() is expected 3 or 4, but given {}.".format(mixture.dim()))
+        
+        # Use soft mask
+        ratio = estimated_sources_amplitude / (estimated_sources_amplitude.sum(dim=0) + eps)
+        estimated_sources = ratio * mixture
+
+        norm = max(1, torch.abs(mixture).max() / 10)
+        mixture, estimated_sources = mixture / norm, estimated_sources / norm
+
+        estimated_sources = update_em(mixture, estimated_sources, iteration, eps=eps)
+        estimated_sources = norm * estimated_sources
+    elif n_dims == 5:
+        """
+        Shape of mixture is (batch_size, 1, n_channels, n_bins, n_frames) or (batch_size, n_channels, n_bins, n_frames)
+        """
+        if n_dims_mixture == 5:
+            mixture = mixture.squeeze(dim=1) # (batch_size, n_channels, n_bins, n_frames)
+        elif n_dims_mixture != 4:
+            raise ValueError("mixture.dim() is expected 4 or 5, but given {}.".format(mixture.dim()))
+        
+        estimated_sources = []
+
+        for _mixture, _estimated_sources_amplitude in zip(mixture, estimated_sources_amplitude):
+            # Use soft mask
+            ratio = _estimated_sources_amplitude / (_estimated_sources_amplitude.sum(dim=0) + eps)
+            _estimated_sources = ratio * _mixture
+
+            norm = max(1, torch.abs(_mixture).max() / 10)
+            _mixture, _estimated_sources = _mixture / norm, _estimated_sources / norm
+
+            _estimated_sources = update_em(_mixture, _estimated_sources, iteration, eps=eps)
+            _estimated_sources = norm * _estimated_sources
+
+            estimated_sources.append(_estimated_sources.unsqueeze(dim=0))
+        
+        estimated_sources = torch.cat(estimated_sources, dim=0)
+    else:
+        raise ValueError("estimated_sources_amplitude.dim() is expected 4 or 5, but given {}.".format(estimated_sources_amplitude.dim()))
+
+    return estimated_sources
+
+def update_em(mixture, estimated_sources, iterations=1, source_parallel=False, eps=EPS):
+    """
+    Args:
+        mixture: (n_channels, n_bins, n_frames)
+        estimated_sources: (n_sources, n_channels, n_bins, n_frames)
+    Returns
+        estiamted_sources: (n_sources, n_channels, n_bins, n_frames)
+    """
+    n_sources, n_channels, _, _ = estimated_sources.size()
+
+    for iteration_idx in range(iterations):
+        v, R = [], []
+        Cxx = 0
+
+        if source_parallel:
+            v, R = get_stats(estimated_sources, eps=eps) # (n_sources, n_bins, n_frames), (n_sources, n_bins, n_channels, n_channels)
+            Cxx = torch.sum(v.unsqueeze(dim=4) * R, dim=0) # (n_bins, n_frames, n_channels, n_channels)
+        else:
+            for source_idx in range(n_sources):
+                y_n = estimated_sources[source_idx] # (n_channels, n_bins, n_frames)
+                v_n, R_n = get_stats(y_n, eps=eps) # (n_bins, n_frames), (n_bins, n_channels, n_channels)
+                Cxx = Cxx + v_n.unsqueeze(dim=2).unsqueeze(dim=3) * R_n.unsqueeze(dim=1) # (n_bins, n_frames, n_channels, n_channels)
+                v.append(v_n.unsqueeze(dim=0))
+                R.append(R_n.unsqueeze(dim=0))
+        
+            v, R = torch.cat(v, dim=0), torch.cat(R, dim=0) # (n_sources, n_bins, n_frames), (n_sources, n_bins, n_channels, n_channels)
+       
+        v, R = v.unsqueeze(dim=3), R.unsqueeze(dim=2) # (n_sources, n_bins, n_frames, 1), (n_sources, n_bins, 1, n_channels, n_channels)
+
+        inv_Cxx = torch.linalg.inv(Cxx + math.sqrt(eps) * torch.eye(n_channels)) # (n_bins, n_frames, n_channels, n_channels)
+
+        if source_parallel:
+            gain = v.unsqueeze(dim=4) * torch.sum(R.unsqueeze(dim=5) * inv_Cxx.unsqueeze(dim=2), dim=4) # (n_sources, n_bins, n_frames, n_channels, n_channels)
+            gain = gain.permute(0, 3, 4, 1, 2) # (n_sources, n_channels, n_channels, n_bins, n_frames)
+            estimated_sources = torch.sum(gain * mixture, dim=2) # (n_sources, n_channels, n_bins, n_frames)
+        else:
+            estimated_sources = []
+
+            for source_idx in range(n_sources):
+                v_n, R_n = v[source_idx], R[source_idx] # (n_bins, n_frames, 1), (n_bins, 1, n_channels, n_channels)
+
+                gain_n = v_n.unsqueeze(dim=3) * torch.sum(R_n.unsqueeze(dim=4) * inv_Cxx.unsqueeze(dim=2), dim=3) # (n_bins, n_frames, n_channels, n_channels)
+                gain_n = gain_n.permute(2, 3, 0, 1) # (n_channels, n_channels, n_bins, n_frames)
+                estimated_source = torch.sum(gain_n * mixture, dim=1) # (n_channels, n_bins, n_frames)
+                estimated_sources.append(estimated_source.unsqueeze(dim=0))
+            
+            estimated_sources = torch.cat(estimated_sources, dim=0) # (n_sources, n_channels, n_bins, n_frames)
+
+    return estimated_sources
+
+def get_stats(spectrogram, eps=EPS):
+    """
+    Compute empirical parameters of local gaussian model.
+    Args:
+        spectrogram <torch.Tensor>: (n_mics, n_bins, n_frames) or (n_sources, n_mics, n_bins, n_frames)
+    Returns:
+        psd <torch.Tensor>: (n_bins, n_frames) or (n_sources, n_bins, n_frames)
+        covariance <torch.Tensor>: (n_bins, n_frames, n_mics, n_mics) or (n_sources, n_bins, n_frames, n_mics, n_mics)
+    """
+    n_dims = spectrogram.dim()
+
+    if n_dims == 3:
+        psd = torch.mean(torch.abs(spectrogram)**2, dim=0) # (n_bins, n_frames)
+        covariance = spectrogram.unsqueeze(dim=1) * spectrogram.unsqueeze(dim=0).conj() # (n_mics, n_mics, n_bins, n_frames)
+        covariance = covariance.sum(dim=3) # (n_mics, n_mics, n_bins)
+        denominator = psd.sum(dim=1) + eps # (n_bins,)
+
+        covariance = covariance / denominator # (n_mics, n_mics, n_bins, n_frames)
+        covariance = covariance.permute(2, 0, 1) # (n_bins, n_mics, n_mics)
+    elif n_dims == 4:
+        psd = torch.mean(torch.abs(spectrogram)**2, dim=1) # (n_sources, n_bins, n_frames)
+        covariance = spectrogram.unsqueeze(dim=2) * spectrogram.unsqueeze(dim=1).conj() # (n_sources, n_mics, n_mics, n_bins, n_frames)
+        covariance = covariance.sum(dim=4) # (n_sources, n_mics, n_mics, n_bins)
+        denominator = psd.sum(dim=2) + eps # (n_sources, n_bins)
+        
+        covariance = covariance / denominator.unsqueeze(dim=1).unsqueeze(dim=2) # (n_sources, n_mics, n_mics, n_bins)
+        covariance = covariance.permute(0, 3, 1, 2) # (n_sources, n_bins, n_mics, n_mics)
+    else:
+        raise ValueError("Invalid dimension of tensor is given.")
+
+    return psd, covariance
