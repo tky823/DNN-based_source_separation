1548033a59f6a108b31e36290d4f9448f1d25d09
tky823 <40362510+tky823@users.noreply.github.com>
2021-09-22 16:33:16 +0900

    refactor codes

--- egs/musdb18/conv-tasnet/train.sh
+++ egs/musdb18/conv-tasnet/train.sh
@@ -2,6 +2,7 @@
 
 exp_dir="./exp"
 continue_from=""
+tag=""
 
 sources="[bass,drums,other,vocals]"
 duration=4
@@ -62,7 +63,11 @@ if [ ${enc_bases} = 'Fourier' -o ${dec_bases} = 'Fourier' ]; then
     prefix="${preffix}${window_fn}-window_"
 fi
 
-save_dir="${exp_dir}/${sources}/sr${sr}/${duration}sec/${enc_bases}-${dec_bases}/${criterion}/N${N}_L${L}_B${B}_H${H}_Sc${Sc}_P${P}_X${X}_R${R}/${prefix}dilated${dilated}_separable${separable}_causal${causal}_${sep_nonlinear}_norm${sep_norm}_mask-${mask_nonlinear}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-decay${weight_decay}_clip${max_norm}/seed${seed}"
+if [ -z "${tag}" ]; then
+    save_dir="${exp_dir}/${sources}/sr${sr}/${duration}sec/${enc_bases}-${dec_bases}/${criterion}/N${N}_L${L}_B${B}_H${H}_Sc${Sc}_P${P}_X${X}_R${R}/${prefix}dilated${dilated}_separable${separable}_causal${causal}_${sep_nonlinear}_norm${sep_norm}_mask-${mask_nonlinear}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-decay${weight_decay}_clip${max_norm}/seed${seed}"
+else
+    save_dir="${exp_dir}/${tag}"
+fi
 
 model_dir="${save_dir}/model"
 loss_dir="${save_dir}/loss"
