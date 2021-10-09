32ecae0e3284596aea83331f6f94c84868b6fb1a
tky823 <40362510+tky823@users.noreply.github.com>
2021-10-09 23:18:35 +0900

    update prepare_mss-model.sh

--- egs/tutorials/conv-tasnet/prepare_mss-model.sh
+++ egs/tutorials/conv-tasnet/prepare_mss-model.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-model_id="1yQTdGu2jigAHotJJ7JAQcfqedACCP40t"
+model_id=""
+model_name="sr44100_2sec"
+file="model"
 
 . ./path.sh
 . parse_options.sh || exit 1
 
 echo "Download Conv-TasNet. (Dataset: MUSDB18, sampling frequency 44.1kHz)"
 
-file='archive'
+model_ids=(
+    ["sr44100_2sec"]="1kKQgkrG2FFnVOsuWeSBshFD1rhYT7lcq"
+    ["sr44100_8sec"]="1yQTdGu2jigAHotJJ7JAQcfqedACCP40t"
+)
+
+if [ -z "${model_id}" ] ; then
+    model_id="${model_ids[${model_name}]}"
+fi
 
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${model_id}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
