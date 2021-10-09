f84b0e0662ac0ef51ac52c039c2fdd0d1d159c3e
tky823 <40362510+tky823@users.noreply.github.com>
2021-10-09 17:48:40 +0900

    refactor codes

--- egs/musdb18/cunet/local/train.py
+++ egs/musdb18/cunet/local/train.py
@@ -75,17 +75,17 @@ def main(args):
     model = ConditionedUNet2d(control_net=control_net, unet=unet)
 
     print(model)
-    print("# Parameters: {}".format(model.num_parameters), flush=True)
+    print("# Parameters: {}".format(model.num_parameters))
     
     if args.use_cuda:
         if torch.cuda.is_available():
             model.cuda()
             model = nn.DataParallel(model)
-            print("Use CUDA")
+            print("Use CUDA", flush=True)
         else:
             raise ValueError("Cannot use CUDA.")
     else:
-        print("Does NOT use CUDA")
+        print("Does NOT use CUDA", flush=True)
         
     # Optimizer
     if args.optimizer == 'sgd':
