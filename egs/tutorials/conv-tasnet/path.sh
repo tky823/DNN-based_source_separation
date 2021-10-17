369a34b455c74127502cb585333cce6aad2d3770
tky823 <delta9guitar97@gmail.com>
2021-10-17 13:32:11 +0900

    refactor codes

--- egs/tutorials/conv-tasnet/path.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export PATH="./local:../common:$PATH"
-export PYTHONPATH="../../../src:../common/src:$PYTHONPATH"
--- egs/wham/conv-tasnet/train_separate-clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Use wsj0-mix/conv-tasnet/train.sh instead."
