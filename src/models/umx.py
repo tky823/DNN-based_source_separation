16ddb946017483eeb20308593391d622c78b8663
tky823 <delta9guitar97@gmail.com>
2021-10-21 18:25:39 +0900

    refactor codes

--- src/models/umx.py
+++ src/models/umx.py
@@ -247,7 +247,7 @@ class OpenUnmix(nn.Module):
         for p in self.parameters():
             if p.requires_grad:
                 _num_parameters += p.numel()
-                
+        
         return _num_parameters
 
 """
