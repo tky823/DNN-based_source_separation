c530f518b1b9b68b97c6859df5a40f8a13b0f998
tky823 <delta9guitar97@gmail.com>
2021-10-24 10:48:58 +0900

    add files for Music Demixing Challenge

---
+++ egs/mdx/common/README.md
@@ -0,0 +1,5 @@
+`src/` includes
+- aicrowd_helpers.py
+- music_demixing.py
+
+They are provided by Music Demixing Challenge 2021.
---
+++ egs/mdx/umx/local/test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# This file uses Open-Unmix for music demixing.
+#
+import argparse
+
+from adhoc_predictor import UMXPredictor
+
+parser = argparse.ArgumentParser(description="Evaluation of Open-Unmix")
+
+parser.add_argument('--sr', type=int, default=44100, help='Sampling rate.')
+parser.add_argument('--duration', type=float, default=256, help='Duration')
+parser.add_argument('--fft_size', type=int, default=4096, help='FFT length')
+parser.add_argument('--hop_size', type=int, default=1024, help='Hop length')
+parser.add_argument('--window_fn', type=str, default='hann', help='Window function')
+parser.add_argument('--sources', type=str, default="[bass,drums,other,vocals]", help='Source names')
+parser.add_argument('--model_dir', type=str, default='./tmp', help='Path to model.')
+
+def main(args):
+    args.sources = args.sources.replace('[', '').replace(']', '').split(',')
+    patch_samples = int(args.duration * args.sr)
+    args.patch_size = (patch_samples + 2 * (args.fft_size // 2) - args.fft_size) // args.hop_size - 1
+
+    submission = UMXPredictor(args)
+    submission.run()
+    
+    print("Successfully generated predictions!")
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)
+    main(args)
