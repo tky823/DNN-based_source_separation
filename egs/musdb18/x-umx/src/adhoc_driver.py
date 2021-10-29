3854b1deea3d46b9aab5f265de461b12603a9d6f
tky823 <delta9guitar97@gmail.com>
2021-10-30 00:04:22 +0900

    refactor codes

--- egs/musdb18/x-umx/src/adhoc_driver.py
+++ egs/musdb18/x-umx/src/adhoc_driver.py
@@ -129,8 +129,15 @@ class AdhocSchedulerTrainer(TrainerBase):
             model_path = os.path.join(self.model_dir, "last.pth")
             self.save_model(epoch, model_path)
             
-            save_path = os.path.join(self.loss_dir, "loss.png")
-            draw_loss_curve(train_loss=self.train_loss[:epoch + 1], valid_loss=self.valid_loss[:epoch + 1], save_path=save_path)
+            if self.combination:
+                save_path = os.path.join(self.loss_dir, "loss.png")
+                draw_loss_curve(train_loss=self.train_loss[:epoch + 1], valid_loss=self.valid_loss[:epoch + 1], save_path=save_path)
+            else:
+                for idx, target in enumerate(self.sources):
+                    save_dir = os.path.join(self.loss_dir, target)
+                    os.makedirs(save_dir, exist_ok=True)
+                    save_path = os.path.join(save_dir, "loss.png")
+                    draw_loss_curve(train_loss=self.train_loss[:epoch + 1, idx], valid_loss=self.valid_loss[:epoch + 1, idx], save_path=save_path)
         
     def run_one_epoch_train(self, epoch):
         # Override
