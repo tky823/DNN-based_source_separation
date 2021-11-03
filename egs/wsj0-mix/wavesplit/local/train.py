aaf9b595e3227a01765a4d6cd5f2df813a6f514a
tky823 <delta9guitar97@gmail.com>
2021-11-04 02:41:02 +0900

    add train.py

---
+++ egs/wsj0-mix/wavesplit/local/train.py
@@ -0,0 +1,4 @@
+from adhoc_utils import create_spk_to_idx
+
+def main():
+    spk_to_idx = create_spk_to_idx(train_list_path)
---
+++ egs/wsj0-mix/wavesplit/src/adhoc_utils.py
@@ -0,0 +1,26 @@
+class SpeakerToIndex:
+    def __init__(self):
+        self.table = {}
+    
+    def add_speaker(self, speaker_id: str):
+        if not speaker_id in self.table:
+            self.table[speaker_id] = len(self.table)
+    
+    def __call__(self, speaker_id: str):
+        return self.table[speaker_id]
+    
+    def __len__(self):
+        return len(self.table)
+
+def create_spk_to_idx(list_path):
+    spk_to_idx = SpeakerToIndex()
+
+    with open(list_path) as f:
+        for line in f:
+            ID = line.strip()
+            spk_ids = ID.split('_')
+            spk_ids = spk_ids[0::2]
+            for spk_id in spk_ids:
+                spk_to_idx.add_speaker(spk_id)
+    
+    return spk_to_idx
