ef4f341fcfa12a32b402a04333c6051659991ede
tky823 <delta9guitar97@gmail.com>
2021-11-04 00:56:10 +0900

    update WaveSplit

--- src/models/wavesplit.py
+++ src/models/wavesplit.py
@@ -1,3 +1,5 @@
+import itertools
+
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -6,12 +8,12 @@ from models.film import FiLM1d
 
 EPS = 1e-12
 
-class WaveSplit(nn.Module):
+class WaveSplitBase(nn.Module):
     def __init__(self, in_channels, latent_dim=512, kernel_size=3, sep_num_blocks=4, sep_num_layers=10, spk_num_layers=14, dilated=True, separable=True, causal=False, nonlinear=None, norm=True, n_sources=2, n_training_sources=None, eps=EPS):
         super().__init__()
 
         self.embed_sources = nn.Embedding(n_training_sources, latent_dim)
-        self.mask = nn.Parameter(1 - torch.eye(n_sources), requeires_grad=False)
+        self.mask = nn.Parameter(1 - torch.eye(n_sources), requires_grad=False)
 
         self.speaker_stack = SpeakerStack(
             in_channels, latent_dim=latent_dim,
@@ -28,6 +30,11 @@ class WaveSplit(nn.Module):
             n_sources=n_sources,
             eps=eps
         )
+
+        all_speaker_id = torch.arange(n_training_sources).long()
+        self.all_speaker_id = nn.Parameter(all_speaker_id, requires_grad=False)
+
+        self.n_sources, self.n_training_sources = n_sources, n_training_sources
     
     def forward(self, input, speaker_id=None):
         """
@@ -36,52 +43,199 @@ class WaveSplit(nn.Module):
         Returns:
             output (batch_size, 1, T)
         """
-        speaker_vector = self.speaker_stack(input) # (batch_size, n_sources, latent_dim, T) TODO
-        speaker_centroids = speaker_vector.mean(dim=-1) # (batch_size, n_sources, latent_dim) for experimentally
-        output = self.sepatation_stack(input, speaker_centroids)
+        raise NotImplementedError("Implement forward.")
+
+class WaveSplit(WaveSplitBase):
+    def __init__(self, in_channels, latent_dim=512, kernel_size=3, sep_num_blocks=4, sep_num_layers=10, spk_num_layers=14, dilated=True, separable=True, causal=False, nonlinear=None, norm=True, n_sources=2, n_training_sources=None, eps=EPS):
+        super().__init__(
+            in_channels, latent_dim=latent_dim,
+            kernel_size=kernel_size,
+            sep_num_blocks=sep_num_blocks, sep_num_layers=sep_num_layers, spk_num_layers=spk_num_layers,
+            dilated=dilated, separable=separable, causal=causal,
+            nonlinear=nonlinear, norm=norm,
+            n_sources=n_sources, n_training_sources=n_training_sources,
+            eps=eps
+        )
+
+        zero_dim_size = ()
+        self.scale, self.bias = nn.Parameter(torch.empty(zero_dim_size), requires_grad=True), nn.Parameter(torch.empty(zero_dim_size), requires_grad=True)
+
+        self._reset_parameters()
+    
+    def _reset_parameters(self):
+        self.scale.data.fill_(1)
+        self.bias.data.fill_(0)
+    
+    def forward(self, input, speaker_id=None, return_all=False, return_speaker_vector=False, stack_dim=1):
+        """
+        Only supports training time
+        Args:
+            input (batch_size, 1, T)
+        Returns:
+            output (batch_size, num_blocks * num_layers, 1, T) if stack_dim=1.
+            sorted_speaker_vector: (batch_size, n_sources, latent_dim, T)
+        """
+        output, sorted_speaker_vector = self.extract_latent(input, speaker_id=speaker_id, return_all=return_all, stack_dim=stack_dim)
+
+        if return_speaker_vector:
+            return output, sorted_speaker_vector
 
         return output
     
-    def compute_speaker_loss(self, speaker_vector, speaker_id, batch_mean=True):
+    def extract_latent(self, input, speaker_id=None, return_all=False, stack_dim=1):
         """
+        Only supports training time
         Args:
-            speaker_vector: (batch_size, n_sources, latent_dim, T)
-            speaker_id: (batch_size, n_sources)
+            input: (batch_size, 1, T)
         Returns:
-            loss: (batch_size,) or ()
+            output: (batch_size, num_blocks * num_layers, 1, T) if stack_dim=1.
+            sorted_speaker_vector: (batch_size, n_sources, latent_dim, T)
         """
-        loss_distance = self.compute_speaker_distance(speaker_vector, speaker_id)
-        loss_local = 0 # self.compute_local_classification(speaker_vector, speaker_id)
-        loss_global = 0 # self.compute_global_classification(speaker_vector, speaker_id)
+        speaker_vector = self.speaker_stack(input) # (batch_size, n_sources, latent_dim, T)
 
-        return loss_distance + loss_local + loss_global
+        speaker_vector = speaker_vector.permute(0, 3, 1, 2).contiguous() # (batch_size, T, n_sources, latent_dim)
+
+        if self.training:
+            speaker_embedding = self.embed_sources(speaker_id) # (batch_size, n_sources, latent_dim)
+            all_speaker_embedding = self.embed_sources(self.all_speaker_id) # (n_training_sources, latent_dim)
+
+            batch_size, T, n_sources, latent_dim = speaker_vector.size()
+
+            with torch.no_grad():
+                _, sorted_idx = self.compute_pit_speaker_loss(speaker_vector, speaker_embedding, all_speaker_embedding, batch_mean=False) # (batch_size, T, n_sources)
+
+                sorted_idx = sorted_idx.view(batch_size * T, n_sources)
+                flatten_sorted_idx = sorted_idx + torch.arange(0, batch_size * T * n_sources, n_sources).long().unsqueeze(dim=-1)
+                flatten_sorted_idx = flatten_sorted_idx.view(batch_size * T * n_sources)
+
+            flatten_speaker_vector = speaker_vector.view(batch_size * T * n_sources, latent_dim)
+            flatten_speaker_vector = flatten_speaker_vector[flatten_sorted_idx]
+            sorted_speaker_vector = flatten_speaker_vector.view(batch_size, T, n_sources, latent_dim)
+            sorted_speaker_vector = sorted_speaker_vector.permute(0, 2, 3, 1).contiguous() # (batch_size, n_sources, latent_dim, T)
+            speaker_centroids = sorted_speaker_vector.mean(dim=-1) # (batch_size, n_sources, latent_dim)
+        else:
+            raise NotImplementedError("Not support test time process.")
+        
+        output = self.sepatation_stack(input, speaker_centroids, return_all=True, stack_dim=stack_dim)
+
+        return output, sorted_speaker_vector
     
-    def compute_speaker_distance(self, speaker_vector, speaker_id):
+    def compute_euclid_distance(self, input, target, dim=-1, keepdim=False, rescale=False):
+        distance = torch.sum((input - target)**2, dim=dim, keepdim=keepdim)
+
+        if rescale:
+            distance = torch.abs(self.scale) * distance + self.bias
+        
+        return distance
+    
+    def compute_speaker_loss(self, speaker_vector, speaker_embedding, all_speaker_embedding, feature_last=True, batch_mean=True):
         """
         Args:
-            speaker_vector: (batch_size, n_sources, latent_dim, T)
-            speaker_id: (batch_size, n_sources)
+            speaker_vector: (batch_size, T, n_sources, latent_dim)
+            speaker_embedding: (batch_size, n_sources, latent_dim)
+            all_speaker_embedding: (n_training_sources, latent_dim)
         Returns:
             loss: (batch_size, T) or (T,)
         """
+        assert feature_last, "feature_last should be True."
+
+        loss_distance = self.compute_speaker_distance(speaker_vector, speaker_embedding, feature_last=feature_last, batch_mean=False) # (batch_size, T, n_sources)
+
+        rescaled_distance = self.compute_euclid_distance(speaker_vector, speaker_embedding.unsqueeze(dim=1), dim=-1, rescale=True) # (batch_size, T, n_sources)
+        rescaled_all_distance = self.compute_euclid_distance(speaker_vector.unsqueeze(dim=3), all_speaker_embedding, dim=-1, rescale=True) # (batch_size, T, n_sources, n_training_sources)
+
+        loss_local = self.compute_local_classification(rescaled_distance, batch_mean=False) # (batch_size, T, n_sources)
+        loss_global = self.compute_global_classification(rescaled_distance, rescaled_all_distance, batch_mean=False) # (batch_size, T, n_sources)
+        loss = torch.sum(loss_distance + loss_local + loss_global, dim=2)
+
+        if batch_mean:
+            loss = loss.mean(dim=0)
+
+        return loss
+    
+    def compute_speaker_distance(self, speaker_vector, speaker_embedding, feature_last=True, batch_mean=True):
+        """
+        Args:
+            speaker_vector: (batch_size, T, n_sources, latent_dim)
+            speaker_embedding: (batch_size, n_sources, latent_dim)
+        Returns:
+            loss: (batch_size, T, n_sources)
+        """
+        assert feature_last, "feature_last should be True."
+
         mask = self.mask # (n_sources, n_sources)
-        embedding = self.embed_sources(speaker_id) # (batch_size, n_sources, latent_dim)
 
-        loss = (speaker_vector - embedding.unsqueeze(dim=-1))**2
-        loss = loss.sum(dim=2) # (batch_size, n_sources, T)
+        loss_distance = self.compute_euclid_distance(speaker_vector, speaker_embedding.unsqueeze(dim=1), dim=-1, rescale=False) # (batch_size, T, n_sources)
 
-        loss_table = speaker_vector.unsqueeze(dim=2) - speaker_vector.unsqueeze(dim=1)
-        loss_table = F.relu(1 - torch.sum(loss_table**2, dim=-2)) # (batch_size, n_sources, n_sources, T)
-        loss_table = torch.sum(mask.unsqueeze(dim=2) * loss_table, dim=2) # (batch_size, n_sources, T)
+        distance_table = self.compute_euclid_distance(speaker_vector.unsqueeze(dim=3), speaker_vector.unsqueeze(dim=2), dim=-1, rescale=False) # (batch_size, T, n_sources, n_sources)
+        loss_hinge = F.relu(1 - distance_table) # (batch_size, T, n_sources, n_sources)
+        loss_hinge = torch.sum(mask * loss_hinge, dim=2) # (batch_size, T, n_sources)
 
-        return loss + loss_table
+        loss = loss_distance + loss_hinge
 
-    def compute_local_classification(self, speaker_vector, speaker_id):
-        return 0
+        if batch_mean:
+            loss = loss.mean(dim=0)
 
-    def compute_global_classification(self, speaker_vector, speaker_id):
-        return 0
+        return loss
 
+    def compute_local_classification(self, distance, batch_mean=True):
+        """
+        Args:
+            distance: (batch_size, T, n_sources)
+        Returns:
+            loss: (batch_size, T, n_sources)
+        """
+        loss = distance + torch.logsumexp(- distance, dim=2, keepdim=True) # (batch_size, T, n_sources)
+
+        if batch_mean:
+            loss = loss.mean(dim=0)
+
+        return loss
+
+    def compute_global_classification(self, distance, all_distance, batch_mean=True):
+        """
+        Args:
+            distance: (batch_size, T, n_sources)
+            all_distance: (batch_size, T, n_sources, n_training_sources)
+        Returns:
+            loss: (batch_size, T, n_sources)
+        """
+        loss = distance + torch.logsumexp(- all_distance, dim=3) # (batch_size, T, n_sources)
+
+        if batch_mean:
+            loss = loss.mean(dim=0)
+
+        return loss
+    
+    def compute_pit_speaker_loss(self, speaker_vector, speaker_embedding, all_speaker_embedding, feature_last=True, batch_mean=True):
+        """
+        Args:
+            speaker_vector: (batch_size, T, n_sources, latent_dim)
+            speaker_embedding: (batch_size, n_sources, latent_dim)
+            all_speaker_embedding: (batch_size, n_training_sources, latent_dim)
+        Returns:
+            loss: (batch_size, T) or (T,)
+        """
+        assert feature_last, "feature_last should be True."
+
+        patterns = list(itertools.permutations(range(self.n_sources)))
+        patterns = torch.Tensor(patterns).long()
+        
+        P = len(patterns)
+        possible_loss = []
+        
+        for idx in range(P):
+            pattern = patterns[idx]
+            loss = self.compute_speaker_loss(speaker_vector[:, :, pattern], speaker_embedding, all_speaker_embedding, feature_last=feature_last, batch_mean=False) # (batch_size, T)
+            possible_loss.append(loss)
+        
+        possible_loss = torch.stack(possible_loss, dim=2) # (batch_size, T, P)
+        loss, indices = torch.min(possible_loss, dim=2) # loss (batch_size, T), indices (batch_size, T)
+        
+        if batch_mean:
+            loss = loss.mean(dim=0)
+        
+        return loss, patterns[indices]
 
 class SpeakerStack(nn.Module):
     def __init__(self, in_channels, latent_dim=512, kernel_size=3, num_layers=14, dilated=True, separable=True, causal=False, nonlinear=None, norm=True, n_sources=2, eps=EPS):
@@ -149,6 +303,7 @@ class SeparationStack(nn.Module):
         for block_idx in range(num_blocks):
             subnet = []
             sub_fc_weights, sub_fc_biases = [], []
+
             for layer_idx in range(num_layers):
                 if dilated:
                     dilation = 2**layer_idx
@@ -160,15 +315,13 @@ class SeparationStack(nn.Module):
                 if block_idx == 0 and layer_idx == 0:
                     residual = True
                     _in_channels = in_channels
-                    _out_channels = latent_dim
                 elif block_idx == num_blocks - 1 and layer_idx == num_layers - 1:
                     residual = False
-                    _in_channels = latent_dim
-                    _out_channels = n_sources
+                    _in_channels = n_sources
                 else:
                     residual = True
-                    _in_channels = latent_dim
-                    _out_channels = latent_dim
+                    _in_channels = n_sources
+                _out_channels = n_sources
                 
                 block = FiLMConvBlock1d(
                     _in_channels, _out_channels, hidden_channels=latent_dim,
@@ -197,14 +350,14 @@ class SeparationStack(nn.Module):
 
         self.net = nn.Sequential(*net)
 
-    def forward(self, input, speaker_centroids):
+    def forward(self, input, speaker_centroids, return_all=False, stack_dim=1):
         """
         Args:
             input (batch_size, in_channels, T)
             speaker_centroids (batch_size, n_sources, latent_dim)
         """
-        n_sources = self.n_sources
         x = input
+        output = []
         
         for block_idx in range(self.num_blocks):
             fc_weights_block = self.fc_weights[block_idx]
@@ -214,8 +367,12 @@ class SeparationStack(nn.Module):
                 gamma = fc_weights_block[layer_idx](speaker_centroids)
                 beta = fc_biases_block[layer_idx](speaker_centroids)
                 x = net_block[layer_idx](x, gamma, beta)
+                output.append(x)
         
-        output = x
+        if return_all:
+            output = torch.stack(output, dim=stack_dim)
+        else:
+            output = output[-1]
 
         return output
 
@@ -444,19 +601,26 @@ def _test():
 
 def _test_wavesplit():
     batch_size = 4
-    n_sources = 2
+    n_training_sources, n_sources = 10, 2
     in_channels = 1
     latent_dim = 512
     T = 1024
     input = torch.randn(batch_size, in_channels, T)
+    target = torch.randn(batch_size, n_sources, T)
+    speaker_id = torch.randint(0, n_training_sources, (batch_size, n_sources))
 
-    model = WaveSplit(in_channels, latent_dim, n_sources=n_sources)
-    output = model(input)
+    model = WaveSplit(in_channels, latent_dim, n_sources=n_sources, n_training_sources=n_training_sources)
+    output, sorted_speaker_vector = model(input, speaker_id=speaker_id, return_all=True, return_speaker_vector=True, stack_dim=1)
 
-    print(input.size(), output.size())
+    loss = - sisdr(output, target.unsqueeze(dim=1))
+    loss = loss.mean()
+
+    print(input.size(), output.size(), sorted_speaker_vector.size())
 
 if __name__ == '__main__':
     import torch
+
+    from criterion.sdr import sisdr
 
     torch.manual_seed(111)
 
