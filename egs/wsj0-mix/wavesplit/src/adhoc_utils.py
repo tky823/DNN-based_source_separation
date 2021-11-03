d663cb623ae62bb7af30d5b4fcb6063929298616
tky823 <delta9guitar97@gmail.com>
2021-11-04 02:43:29 +0900

    add train.py

--- egs/wsj0-mix/wavesplit/src/adhoc_utils.py
+++ egs/wsj0-mix/wavesplit/src/adhoc_utils.py
@@ -6,8 +6,11 @@ class SpeakerToIndex:
         if not speaker_id in self.table:
             self.table[speaker_id] = len(self.table)
     
-    def __call__(self, speaker_id: str):
-        return self.table[speaker_id]
+    def __call__(self, speaker_id: str, as_tensor=True):
+        spk_idx = self.table[speaker_id]
+        if as_tensor:
+            spk_idx = torch.tensor(spk_idx)
+        return spk_idx
     
     def __len__(self):
         return len(self.table)
