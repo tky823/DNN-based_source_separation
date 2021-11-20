0f63ab62bc0c9dc9b9d0f3849d9998fcb7410082
tky823 <delta9guitar97@gmail.com>
2021-11-20 17:44:30 +0900

    add pretrained SepFormer

--- src/models/sepformer.py
+++ src/models/sepformer.py
@@ -11,6 +11,14 @@ from models.gtu import GTU1d
 
 EPS = 1e-12
 
+__pretrained_model_ids__ = {
+    "wsj0-mix": {
+        8000: {
+            2: "1-2Ta0CdVw78GzmP1QCzvoKLF2xPOz_Kb"
+        }
+    }
+}
+
 class SepFormer(nn.Module):
     def __init__(
         self,
@@ -218,7 +226,47 @@ class SepFormer(nn.Module):
 
     @classmethod
     def build_from_pretrained(cls, root="./pretrained", quiet=False, load_state_dict=True, **kwargs):
-        raise NotImplementedError
+        import os
+        
+        from utils.utils import download_pretrained_model_from_google_drive
+
+        task = kwargs.get('task')
+
+        if not task in __pretrained_model_ids__:
+            raise KeyError("Invalid task ({}) is specified.".format(task))
+            
+        pretrained_model_ids_task = __pretrained_model_ids__[task]
+        additional_attributes = {}
+        
+        if task in ['wsj0-mix', 'wsj0']:
+            sample_rate = kwargs.get('sample_rate') or 8000
+            n_sources = kwargs.get('n_sources') or 2
+            model_choice = kwargs.get('model_choice') or 'best'
+
+            model_id = pretrained_model_ids_task[sample_rate][n_sources]
+            download_dir = os.path.join(root, cls.__name__, task, "sr{}/{}speakers".format(sample_rate, n_sources))
+
+            additional_attributes.update({
+                'n_sources': n_sources
+            })
+        else:
+            raise NotImplementedError("Not support task={}.".format(task))
+        
+        additional_attributes.update({
+            'sample_rate': sample_rate
+        })
+
+        model_path = os.path.join(download_dir, "model", "{}.pth".format(model_choice))
+
+        if not os.path.exists(model_path):
+            download_pretrained_model_from_google_drive(model_id, download_dir, quiet=quiet)
+        
+        model = cls.build_model(model_path, load_state_dict=load_state_dict)
+
+        for key, value in additional_attributes.items():
+            setattr(model, key, value)
+
+        return model
     
     @property
     def num_parameters(self):
