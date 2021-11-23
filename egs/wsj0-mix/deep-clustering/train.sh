f6d2b2e933a98b7e31f40dfdc85b3db23861a7e4
tky823 <delta9guitar97@gmail.com>
2021-11-23 16:10:41 +0900

    fix typo

--- egs/wsj0-mix/deep-clustering/train.sh
+++ egs/wsj0-mix/deep-clustering/train.sh
@@ -67,7 +67,6 @@ if [ -z "${tag}" ]; then
     else
         save_dir="${save_dir}/take_identity"
     fi
-    save_dir="${save_dir}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-decay${weight_decay}_clip${max_norm}/seed${seed}"
     if [ "${optimizer}" = "momentum-sgd"]; then
         save_dir="${save_dir}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-momentum${momentum}-decay${weight_decay}_clip${max_norm}"
     else
