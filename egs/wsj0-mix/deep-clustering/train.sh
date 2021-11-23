4365c3e3c26ee8c5d1bd28541226c33436f52049
tky823 <delta9guitar97@gmail.com>
2021-11-23 16:11:10 +0900

    fix typo

--- egs/wsj0-mix/deep-clustering/train.sh
+++ egs/wsj0-mix/deep-clustering/train.sh
@@ -67,7 +67,7 @@ if [ -z "${tag}" ]; then
     else
         save_dir="${save_dir}/take_identity"
     fi
-    if [ "${optimizer}" = "momentum-sgd"]; then
+    if [ "${optimizer}" = "momentum-sgd" ]; then
         save_dir="${save_dir}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-momentum${momentum}-decay${weight_decay}_clip${max_norm}"
     else
         save_dir="${save_dir}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-decay${weight_decay}_clip${max_norm}"
