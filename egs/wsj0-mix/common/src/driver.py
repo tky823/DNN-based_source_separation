2a8fab71e2eb6f2d29042aee1c2aa1fb904ff9ca
tky823 <delta9guitar97@gmail.com>
2021-12-08 00:24:24 +0900

    refactor codes

--- egs/wsj0-mix/common/src/driver.py
+++ egs/wsj0-mix/common/src/driver.py
@@ -268,7 +268,8 @@ class TesterBase:
 
         print("ID, Loss, Loss improvement, SDR improvement, SIR improvement, SAR, PESQ", flush=True)
 
-        tmp_dir = os.path.join(os.getcwd(), 'tmp')
+        tmp_ID = str(uuid.uuid4())
+        tmp_dir = os.path.join(os.getcwd(), 'tmp-{}'.format(tmp_ID))
         os.makedirs(tmp_dir, exist_ok=True)
         shutil.copy('./PESQ', os.path.join(tmp_dir, 'PESQ'))
         os.chdir(tmp_dir)
