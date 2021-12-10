54c1da42db7508f9565741e6bd21d2abf95e7376
tky823 <delta9guitar97@gmail.com>
2021-12-10 10:43:55 +0900

    refactor codes

--- egs/tutorials/vae/train.sh
+++ egs/tutorials/vae/train.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 exp_dir="./exp"
+continue_from=""
 tag=""
 
 latent_dim=10
@@ -8,24 +9,45 @@ L=1 # Number of samples
 H=200
 R=3
 
-batch_size=100
+optimizer="adam"
 lr=1e-3
+weight_decay=0
+max_norm=0
+
+batch_size=100
 epochs=100
+
+use_cuda=1
+overwrite=0
+seed=42
 
 . ./path.sh
 
 if [ -z "${tag}" ]; then
-    save_dir="${exp_dir}/latent${latent_dim}_L${L}_H${H}_R${R}_b${batch_size}_lr${lr}_epochs${epochs}/seed${seed}"
+    save_dir="${exp_dir}/latent${latent_dim}_L${L}_H${H}_R${R}/b${batch_size}_e${epochs}_${optimizer}-lr${lr}-decay${weight_decay}_clip${max_norm}/seed${seed}"
 else
     save_dir="${exp_dir}/${tag}"
 fi
+
+model_dir="${save_dir}/model"
+loss_dir="${save_dir}/loss"
+sample_dir="${save_dir}/sample"
 
 train.py \
 --latent_dim ${latent_dim} \
 --hidden_channels ${H} \
 --num_layers ${R} \
 --num_samples ${L} \
---batch_size ${batch_size} \
+--optimizer ${optimizer} \
 --lr ${lr} \
+--weight_decay ${weight_decay} \
+--max_norm ${max_norm} \
+--batch_size ${batch_size} \
 --epochs ${epochs} \
---save_dir "${exp_dir}"
+--model_dir "${model_dir}"
+--loss_dir "${loss_dir}"
+--sample_dir "${sample_dir}"
+--continue_from "${continue_from}"
+--use_cuda ${use_cuda}
+--overwrite ${overwrite}
+--seed ${seed}
