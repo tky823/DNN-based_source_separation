a55d7da18e6dba6135e35908b9202c1817208cb9
tky823 <delta9guitar97@gmail.com>
2021-12-10 15:31:31 +0900

    bug fixes

--- egs/tutorials/vae/src/adhoc_utils.py
+++ egs/tutorials/vae/src/adhoc_utils.py
@@ -3,16 +3,16 @@ from models.vae import Decoder as NaiveDecoder
 from models.vae import VAE, NormalLatentSampler
 
 def choose_vae(name, **kwargs):
-    if name == "naive-vae":
+    if name == "naive-normal":
         in_channels, hidden_channels = kwargs["in_channels"], kwargs["hidden_channels"]
         latent_dim = kwargs["latent_dim"]
         num_layers = kwargs["num_layers"]
         encoder = NaiveEncoder(in_channels, hidden_channels, latent_dim=latent_dim, num_layers=num_layers)
         decoder = NaiveDecoder(in_channels, hidden_channels, latent_dim=latent_dim, num_layers=num_layers)
+        latent_sampler = NormalLatentSampler()
     else:
         raise ValueError("Not support model {}".format(name))
     
-    latent_sampler = NormalLatentSampler()
     model = VAE(encoder, decoder, latent_sampler=latent_sampler)
 
     return model
