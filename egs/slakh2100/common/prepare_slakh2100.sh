53e055b4345bde1717e53751e388d147c52f8b42
tky823 <delta9guitar97@gmail.com>
2021-12-11 12:40:29 +0900

    update prepare_slakh2100.sh

--- egs/slakh2100/common/prepare_slakh2100.sh
+++ egs/slakh2100/common/prepare_slakh2100.sh
@@ -11,6 +11,9 @@ if [ ${is_tiny} -eq 0 ]; then
     if [ -z "${slakh2100_root}" ]; then
         slakh2100_root="../../../dataset/slakh2100_flac_redux"
     fi
+    if [ ! -d "${slakh2100_root}" ]; then
+        mkdir -p "${slakh2100_root}"
+    fi
     file="slakh2100_flac_redux.tar.gz"
     wget "https://zenodo.org/record/4599666/files/${file}" -P "/tmp"
     tar -zxvf "/tmp/${file}" -C "/tmp/"
@@ -20,6 +23,9 @@ if [ ${is_tiny} -eq 0 ]; then
 else
     if [ -z "${slakh2100_root}" ]; then
         slakh2100_root="../../../dataset/babyslakh_16k"
+    fi
+    if [ ! -d "${slakh2100_root}" ]; then
+        mkdir -p "${slakh2100_root}"
     fi
     file="babyslakh_16k.tar.gz"
     wget "https://zenodo.org/record/4603870/files/${file}" -P "/tmp"
