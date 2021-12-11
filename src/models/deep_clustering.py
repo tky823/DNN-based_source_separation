2d8cbe2423c779772e0978c23a236294ddd62c7c
tky823 <delta9guitar97@gmail.com>
2021-12-11 10:35:34 +0900

    update pretrained model

--- src/models/deep_clustering.py
+++ src/models/deep_clustering.py
@@ -15,7 +15,8 @@ class DeepEmbedding(nn.Module):
     pretrained_model_ids = {
         "wsj0-mix": {
             8000: {
-                2: "1-34LWrdBKrZpREwHAJfsMkkxeTH5AkBX"
+                2: "1tNL5-3uPfJZY1WOXzMOZkgFpSX1nS4jO",
+                3: "1-ACFRHGFu2S44DxUKj35tCBI0V8W__iQ"
             }
         }
     }
