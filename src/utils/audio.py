cbda14d5d69e26a14b6f55fa05910af3b11eb7e6
tky823 <delta9guitar97@gmail.com>
2021-12-12 02:48:57 +0900

    refactor codes

--- src/utils/audio.py
+++ src/utils/audio.py
@@ -40,7 +40,7 @@ def build_optimal_window(window, hop_length=None):
 def load_piano_roll(midi_path, sample_rate, hop_length, dtype=torch.uint8):
     import pretty_midi
 
-    midi = pretty_midi.PrettyMIDI(path)
+    midi = pretty_midi.PrettyMIDI(midi_path)
     piano_roll_sample_rate = sample_rate / hop_length
     piano_roll = midi.get_piano_roll(fs=piano_roll_sample_rate)
     piano_roll = piano_roll.astype(np.uint8)
@@ -54,3 +54,33 @@ def load_piano_roll(midi_path, sample_rate, hop_length, dtype=torch.uint8):
         raise ValueError("Invalid dtype is specified.")
 
     return piano_roll
+
+def _test_piano_roll():
+    n_fft, hop_length = 4096, 1024
+
+    waveform, sample_rate = torchaudio.load("data/midi/CDEFGGGAFCAGCG.wav")
+    window = torch.hann_window(n_fft)
+    spectrogram = torch.stft(waveform, n_fft, hop_length=hop_length, window=window, center=True, onesided=True, return_complex=True)
+    log_spectrogram = 20 * torch.log10(torch.abs(spectrogram) + 1e-12)
+    piano_roll = load_piano_roll("data/midi/CDEFGGGAFCAGCG.mid", sample_rate=sample_rate, hop_length=hop_length, dtype=torch.float)
+    
+    print(piano_roll.size())
+
+    plt.figure(figsize=(12, 8))
+    plt.pcolormesh(log_spectrogram[0, :500], cmap="jet")
+    plt.savefig("data/midi/CDEFGGGAFCAGCG-spectrogram.png", bbox_inches="tight")
+    plt.close()
+
+    plt.figure(figsize=(12, 8))
+    plt.pcolormesh(piano_roll, cmap="binary")
+    plt.grid()
+    plt.savefig("data/midi/CDEFGGGAFCAGCG-midi.png", bbox_inches="tight")
+    plt.close()
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import torchaudio
+
+    plt.rcParams["font.size"] = 18
+
+    _test_piano_roll()
