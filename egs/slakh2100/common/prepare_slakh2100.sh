94306bc1b997d0eaea90e403f9bd7eaea3b0acdc
tky823 <delta9guitar97@gmail.com>
2021-12-11 12:35:18 +0900

    update prepare_slakh2100.sh

--- egs/slakh2100/common/prepare_slakh2100.sh
+++ egs/slakh2100/common/prepare_slakh2100.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-slakh2100_root="../../../dataset/babyslakh_16k"
+slakh2100_root=""
 is_tiny=0
 
 . ./parse_options.sh || exit 1
@@ -8,6 +8,9 @@ is_tiny=0
 mkdir -p "${slakh2100_root}"
 
 if [ ${is_tiny} -eq 0 ]; then
+    if [ -z "${slakh2100_root}" ]; then
+        slakh2100_root="../../../dataset/slakh2100_flac_redux"
+    fi
     file="slakh2100_flac_redux.tar.gz"
     wget "https://zenodo.org/record/4599666/files/${file}" -P "/tmp"
     tar -zxvf "/tmp/${file}" -C "/tmp/"
@@ -15,6 +18,9 @@ if [ ${is_tiny} -eq 0 ]; then
 
     mv "/tmp/slakh2100_flac_redux/"* "${slakh2100_root}"
 else
+    if [ -z "${slakh2100_root}" ]; then
+        slakh2100_root="../../../dataset/babyslakh_16k"
+    fi
     file="babyslakh_16k.tar.gz"
     wget "https://zenodo.org/record/4603870/files/${file}" -P "/tmp"
     tar -zxvf "/tmp/${file}" -C "/tmp/"
